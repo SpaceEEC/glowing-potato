8eb630d1886c8077d7e3961f36c2a12a7a6eca55
SpaceEEC <spaceeec@yahoo.com>
2017-01-04 04:20:45 +0100

    added command dis/enable ability

---
+++ checks/disabled.js
@@ -0,0 +1,21 @@
+exports.run = (bot, msg, cmd) => new Promise((resolve, reject) => {
+  if (cmd.conf.enabled && !msg.conf.disabledcommands.includes(cmd.help.name)) {
+    resolve();
+  } else if (!cmd.conf.enabled) {
+    reject('Dieser Befehl ist global deaktiviert.');
+  } else if (msg.conf.disabledcmds.includes(cmd.help.name)) {
+    reject('Dieser Befehl ist gildenweit deaktiviert.');
+  } else {
+    bot.log(`
+Unerwarteter Ausgang bei disabled.js:
+cmd.help.name: ${cmd.help.name ? cmd.help.name : msg.content}
+cmd.conf.enabled: ${cmd.conf.enabled ? cmd.conf.enabled : 'falsy'}
+msg.conf.disabledcmds.includes(cmd.help.name): ${msg.conf.disabledcmds.includes(cmd.help.name) ?
+        msg.conf.disabledcmds.includes(cmd.help.name) : 'falsy'}`);
+    reject('Konnte den Deaktivierungsgrund nicht ermitteln.\nNotiz:Diese Fehlermeldung solltest du nicht bekommen.');
+  }
+});
+exports.conf = {
+  spamProtection: false,
+  enabled: true,
+};
---
+++ commands/command.js
@@ -0,0 +1,40 @@
+exports.run = (bot, msg, params = []) => new Promise((resolve, reject) => { // eslint-disable-line
+  if (!params[1] || !['disable', 'enable'].includes(params[0])) {
+    return msg.channel.sendMessage('Fehlender Parameter oder ungültige Operation');
+  }
+  let cmd;
+  if (bot.commands.has(params[1])) {
+    cmd = params[1];
+  } else if (bot.aliases.has(params[1])) {
+    cmd = bot.commands.get(bot.aliases.get(params[1])).help.name;
+  }
+  if (params[0] === 'disable') {
+    return bot.internal.config.add(bot, msg, 'disabledcommands', cmd)
+    .then(() => {
+      msg.channel.sendMessage(`Der Befehl \`${cmd}\` wurde Erfolgreich deaktiviert.`);
+    })
+    .catch(e => msg.channel.sendMessage(e));
+  }
+  if (params[0] === 'enable') {
+    return bot.internal.config.remove(bot, msg, 'disabledcommands', cmd)
+    .then(() => {
+      msg.channel.sendMessage(`Der Befehl \`${cmd}\` wurde Erfolgreich aktiviert.`);
+    })
+    .catch(e => msg.channel.sendMessage(e));
+  }
+});
+
+exports.conf = {
+  group: 'Admincommands',
+  spamProtection: false,
+  enabled: true,
+  aliases: ['cmd'],
+  permLevel: 10,
+};
+
+exports.help = {
+  name: 'command',
+  shortdescription: 'Kurzbeschreibung',
+  description: 'Mit diesem Befehl ist es möglich Befehle Gildenweit zu deaktivieren oder zu reaktivieren.',
+  usage: '$conf.prefixcommand <disable|enable> [Befehl]',
+};
