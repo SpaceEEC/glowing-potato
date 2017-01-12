9876b59895cc067d43e751b4614475b2ba0fbc9e
SpaceEEC <spaceeec@yahoo.com>
2017-01-12 21:09:52 +0100

    fixed that old mistake

--- checks/disabled.js
+++ checks/disabled.js
@@ -3,15 +3,15 @@ exports.run = (bot, msg, cmd) => new Promise((resolve, reject) => {
     resolve();
   } else if (!cmd.conf.enabled) {
     reject('Dieser Befehl ist global deaktiviert.');
-  } else if (msg.conf.disabledcmds.includes(cmd.help.name)) {
+  } else if (msg.conf.disabledcommands.includes(cmd.help.name)) {
     reject('Dieser Befehl ist gildenweit deaktiviert.');
   } else {
     bot.log(`
 Unerwarteter Ausgang bei disabled.js:
 cmd.help.name: ${cmd.help.name ? cmd.help.name : msg.content}
 cmd.conf.enabled: ${cmd.conf.enabled ? cmd.conf.enabled : 'falsy'}
-msg.conf.disabledcmds.includes(cmd.help.name): ${msg.conf.disabledcmds.includes(cmd.help.name)
-        ? msg.conf.disabledcmds.includes(cmd.help.name) : 'falsy'}`);
+msg.conf.disabledcommands.includes(cmd.help.name): ${msg.conf.disabledcommands.includes(cmd.help.name)
+        ? msg.conf.disabledcommands.includes(cmd.help.name) : 'falsy'}`);
     reject('Konnte den Deaktivierungsgrund nicht ermitteln.\nNotiz:Diese Fehlermeldung solltest du nicht bekommen.');
   }
 });
