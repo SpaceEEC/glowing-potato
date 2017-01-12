ead9039d0a68b673f12fb9f628d72f67fba02460
Pascal <wermail2002-kap@yahoo.de>
2017-01-12 01:11:40 +0100

    prunemembercommand inb4 ratelimits

---
+++ commands/prunemembers.js
@@ -0,0 +1,31 @@
+exports.run = (bot, msg, params = []) => new Promise((resolve, reject) => { // eslint-disable-line
+  const kickmembers = msg.guild.members.filter(m => m.roles.size === 1);
+  if (kickmembers.size === 0) {
+    return msg.channel.sendMessage('Es gibt keine Member ohne mindestens eine Rolle auf diesem Server hier.');
+  }
+  msg.channel.sendMessage(`Kicke ${kickmembers.size} Member ohne Rollen.`);
+  const kicks = [true];
+  kickmembers.map((m) => kicks.push(m.kick()));
+  Promise.all(kicks).then(() => {
+    msg.channel.sendMessage(`${kickmembers.size} Member gekickt.`);
+  }, (reason) => {
+    msg.channel.sendMessage(`Fehler beim kicken:\n\n\`\`\`js\n${reason}\`\``);
+  });
+});
+
+
+exports.conf = {
+  group: 'Adminstuff',
+  spamProtection: false,
+  enabled: true,
+  aliases: [],
+  permLevel: 11,
+};
+
+
+exports.help = {
+  name: 'prunemembers',
+  shortdescription: 'Member kicken',
+  description: 'Kickt alle Member, welche keine Rolle haben.',
+  usage: '$conf.prefixprunemembers',
+};
