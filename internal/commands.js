621db82b5ffdd75c49bbc016d50f17cec2890305
Pascal <wermail2002-kap@yahoo.de>
2017-01-20 17:58:57 +0100

    changed command loader so it will accept a directory to read from

--- internal/commands.js
+++ internal/commands.js
@@ -1,26 +1,22 @@
 const fs = require('fs-extra-promise');
-exports.init = (bot) => new Promise((resolve, reject) => { // eslint-disable-line
-  fs.readdirAsync('./commands/')
+exports.init = (bot, dir = './commands/') => new Promise((resolve, reject) => { // eslint-disable-line
+  fs.readdirAsync(dir)
     .then((files) => {
-      try {
-        files = files.filter(f => f.slice(-3) === '.js');
-        bot.log(`Lade insgesamt ${files.length} Befehle.`);
-        files.forEach(f => {
-          try {
-            const props = require(`../commands/${f}`);
-            // bot.log(`Lade Befehl: ${props.help.name}`); // spam
-            bot.commands.set(props.help.name, props);
-            props.conf.aliases.forEach(alias => {
-              bot.aliases.set(alias, props.help.name);
-            });
-          } catch (e) {
-            bot.err(`Fehler beim Laden von ${f}.js\n${e.stack ? e.stack : e}`);
-          }
-        });
-        resolve();
-      } catch (e) {
-        reject(e.stack ? e.stack : e);
-      }
+      files = files.filter(f => f.slice(-3) === '.js');
+      bot.log(`Lade insgesamt ${files.length} Befehle.`);
+      files.forEach(f => {
+        try {
+          const props = require(`.${dir + f}`);
+          // bot.log(`Lade Befehl: ${props.help.name}`); // spam
+          bot.commands.set(props.help.name, props);
+          props.conf.aliases.forEach(alias => {
+            bot.aliases.set(alias, props.help.name);
+          });
+        } catch (e) {
+          bot.err(`Fehler beim Laden von ${dir + f}.js\n${e.stack ? e.stack : e}`);
+        }
+      });
+      resolve();
     })
     .catch((e) => {
       reject(e);
@@ -32,6 +28,7 @@ exports.reload = (bot, command) => new Promise((resolve, reject) => { // eslint-
   try {
     delete require.cache[require.resolve(`../commands/${command}`)];
     const cmd = require(`../commands/${command}`);
+    command = cmd.help.name;
     bot.commands.delete(command);
     bot.aliases.forEach((cmd2, alias) => {
       if (cmd2 === command) bot.aliases.delete(alias);
