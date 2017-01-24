ad84bd6d135fb8ca940b930e5906b1053da80233
SpaceEEC <spaceeec@yahoo.com>
2017-01-24 04:55:54 +0100

    fixes

--- commands/abgespaced/update.js
+++ commands/abgespaced/update.js
@@ -3,18 +3,21 @@ exports.run = async (bot, msg, params = []) => { // eslint-disable-line
   const exec = require('child_process');
   exec.exec('git pull', async (error, stdout, stderr) => {
     if (error) {
-      status_msg.edit(`
-${stdout ? `\`STDOUT\`\n\`\`\`xl\n${stdout}\`\`\`` : ''}
-${stderr ? `\`STERR\`\n\`\`\`xl\n${stderr}\`\`\`` : ''}
+      await status_msg.edit(`
+\`INFO\`\n\`\`\`xl\n${stdout}\n\n${stderr}\`\`\`
 ${error.code ? `Exit Code: ${error.code}` : ''}
 ${error.signal ? `Signal erhalten: ${error.signal}` : ''}
 `);
-    } else {
-      await status_msg.edit(`
-${stdout ? `\`STDOUT\`\n\`\`\`xl\n${stdout}\`\`\`` : ''}
-Starte neu...
-`);
       process.exit(1335);
+    } else {
+      status_msg.edit(`
+${stdout ? `\`STDOUT\`\n\`\`\`xl\n${stdout}\`\`\`` : ''}
+Kein Fehler, dies bedeutet vermutlich kein Update.
+Starte nicht automatisch neu.
+    `).then(mes => {
+      mes.delete(5000);
+      msg.delete(5000);
+    });
     }
   });
 };
