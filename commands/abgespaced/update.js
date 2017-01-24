b6239702306f606f23aacae62804074687922190
SpaceEEC <spaceeec@yahoo.com>
2017-01-24 17:15:20 +0100

    fixed update command

--- commands/abgespaced/update.js
+++ commands/abgespaced/update.js
@@ -3,21 +3,19 @@ exports.run = async (bot, msg, params = []) => { // eslint-disable-line
   const exec = require('child_process');
   exec.exec('git pull', async (error, stdout, stderr) => {
     if (error) {
-      await status_msg.edit(`
+      status_msg.edit(`
 \`INFO\`\n\`\`\`xl\n${stdout}\n\n${stderr}\`\`\`
 ${error.code ? `Exit Code: ${error.code}` : ''}
 ${error.signal ? `Signal erhalten: ${error.signal}` : ''}
-`);
-      process.exit(1335);
+
+Update mit Fehlern ausgeführt, starte nicht automatisch neu...`);
     } else {
-      status_msg.edit(`
+      await status_msg.edit(`
 ${stdout ? `\`STDOUT\`\n\`\`\`xl\n${stdout}\`\`\`` : ''}
 Kein Fehler, dies bedeutet vermutlich kein Update.
-Starte nicht automatisch neu.
-    `).then(mes => {
-      mes.delete(5000);
-      msg.delete(5000);
-    });
+Starte automatisch neu damit das Update wirksam wird....
+    `);
+      process.exit(1335);
     }
   });
 };
