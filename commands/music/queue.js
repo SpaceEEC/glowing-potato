f8b56c948d07547ab9e058993ad006a0b3b8bf38
SpaceEEC <spaceeec@yahoo.com>
2017-01-27 22:24:55 +0100

    queue returns only one field and has option for more fields

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -4,7 +4,7 @@ exports.run = (bot, msg, params = []) => new Promise((resolve, reject) => { // e
       .then(mes => mes.delete(30000));
   } else {
     const musik = bot.internal.musik.get(msg.guild.id);
-    msg.channel.send(musik.queue())
+    msg.channel.send(musik.queue(params[0]))
       .then((mes) => mes.delete(30000));
   }
 });
