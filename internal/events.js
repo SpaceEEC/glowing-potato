48595a60435e8ca57034e95c8673d020f92d4195
SpaceEEC <spaceeec@yahoo.com>
2017-01-30 19:49:20 +0100

    removed debug logger

--- internal/events.js
+++ internal/events.js
@@ -6,9 +6,7 @@ events = Object.keys(events).map(k => events[k]);
 
 exports.init = (bot) => new Promise((resolve, reject) => {
   fs.readdirAsync('./events/').then((files) => {
-    bot.debug(files.map(f => f));
     files = files.filter((f) => events.includes(f.split('.')[0]));
-    bot.debug(files.map(f => f));
     files.forEach((f) => {
       bot.on(f.split('.')[0], (...args) => require(`../events/${f}`).run(bot, ...args));
     });
