3ac93b243f21bcaa7cb4483bf7b60a913d5570ae
Pascal <wermail2002-kap@yahoo.de>
2017-02-01 02:10:27 +0100

    better handling of unhandled rejections

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ bot.internal.init(bot);
 
 
 process.on('unhandledRejection', (err) => {
-  bot.err(`Uncaught Promise Error:\n${err.stack ? err.stack : err}`);
+  if (err.response && err.response.res && err.response.res.text) bot.err(`Uncaught Promise Error:\n$ ${err.response.res.text}`);
+  else bot.err(`Uncaught Promise Error:\n${err.stack ? err.stack : err}`);
 });
 
 
