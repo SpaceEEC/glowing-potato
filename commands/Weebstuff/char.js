5ea1d383e41393dd4f732d2fc864fc3c50abf871
SpaceEEC <spaceeec@yahoo.com>
2017-02-05 03:31:08 +0100

    added prefix for logger

--- commands/Weebstuff/char.js
+++ commands/Weebstuff/char.js
@@ -61,7 +61,7 @@ async function authcheck(bot, msg, params) {
     bot.config.ani_token = res.body.access_token;
     bot.config.ani_expires = res.body.expires;
     bot.debug(`UPDATE config SET ani_expires=${res.body.expires}, ani_token=${res.body.access_token};`);
-    await bot.db.run(`UPDATE config SET ani_expires=?, ani_token=?;`, [res.body.expires, res.body.access_token]);
+    await bot.db.run(`[char] UPDATE config SET ani_expires=?, ani_token=?;`, [res.body.expires, res.body.access_token]);
     // await bot.db.run(`UPDATE config SET ani_token=?`, []);
     // await bot.db.run("UPDATE 'config' SET 'ani_expires'=? AND 'ani_token'=?; WHERE true", [res.body.expires, res.body.access_token]);
     await message.edit('', {
