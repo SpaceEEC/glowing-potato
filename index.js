aa0902d7fc3d14f7039b70dfd56cc535aca75fba
SpaceEEC <spaceeec@yahoo.com>
2017-02-06 18:53:39 +0100

    allowed console in eslint, removed disable lines

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const chalk = require('chalk');
 bot.clk = new chalk.constructor({ enabled: true });
 
 // log stuff
-bot.debug = (msg) => console.log(`[${bot.clk.cyan(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);  // eslint-disable-line
-bot.info = (msg) => console.log(`[${bot.clk.magenta(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);  // eslint-disable-line
-bot.log = (msg) => console.log(`[${bot.clk.green(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);  // eslint-disable-line
-bot.warn = (msg) => console.warn(`[${bot.clk.yellow(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`); // eslint-disable-line
-bot.err = (msg) => console.error(`[${bot.clk.red(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`); // eslint-disable-line
+bot.debug = (msg) => console.log(`[${bot.clk.cyan(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);
+bot.info = (msg) => console.log(`[${bot.clk.magenta(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);
+bot.log = (msg) => console.log(`[${bot.clk.green(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);
+bot.warn = (msg) => console.warn(`[${bot.clk.yellow(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);
+bot.err = (msg) => console.error(`[${bot.clk.red(`${moment().format('DD.MM.YYYY HH:mm:ss')}`)}]: ${msg}`);
 bot.inspect = (obj, hidden = false, depth = 0) => require('util').inspect(obj, hidden, depth);
 
 // methods
