50c7de4da99e880e067eea001a110b7c6145c060
Pascal <wermail2002-kap@yahoo.de>
2017-02-09 15:20:49 +0100

    changed to new permission and channel verify method

--- commands/Musik/skip.js
+++ commands/Musik/skip.js
@@ -1,7 +1,5 @@
 exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unused-vars
-  if (msg.permlvl >= 5
-    || ((!msg.conf.musicrole || (msg.conf.musicrole && msg.member.roles.has(msg.conf.musicrole)))
-      && (!msg.conf.musicchannel || (msg.conf.musicchannel && msg.channel.id === msg.conf.musicchannel)))) {
+  if (bot.internal.music.perms(msg)) {
     if (!bot.internal.musik.get(msg.guild.id)) {
       msg.channel.send('Zur Zeit wird leider nichts gespielt.')
         .then(mes => mes.delete(30000));
@@ -10,9 +8,7 @@ exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unuse
       if (!msg.guild.member(bot.user).voiceChannel) {
         msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
           .then((mes) => mes.delete(5000));
-      } else if (msg.guild.member(bot.user).voiceChannel
-        && (msg.guild.member(bot.user).voiceChannel.id
-          !== msg.member.voiceChannel.id)) {
+      } else if (!bot.internal.music.channel(bot, msg)) {
         msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
           .then((mes) => mes.delete(5000));
       } else {
