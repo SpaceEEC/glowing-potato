31a266ddc41eb7cf5b7e9030c09e18a598c86ebf
SpaceEEC <spaceeec@yahoo.com>
2017-02-09 19:03:54 +0100

    changed skip to embed and added more error handling

--- commands/Musik/skip.js
+++ commands/Musik/skip.js
@@ -12,13 +12,17 @@ exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unuse
     } else {
       const musik = bot.internal.musik.get(msg.guild.id);
       if (musik._music.queue.length === 0 || !musik._music.disp) {
-        msg.channel.sendMessage('Die Queue ist leer, da gibt es nichts zu skippen.');
+        msg.channel.sendMessage('Die Queue ist leer, da gibt es nichts zu skippen.\nOder die Intialisierungsphase ist noch nicht vollendet, dies dauert einen kleinen moment.');
+      } else {
+        msg.channel.sendEmbed(new bot.methods.Embed().setColor(0xff0000).setThumbnail(musik._music.queue[0].info.iurl)
+          .setAuthor(`${msg.member.displayName} hat geskippt:`, msg.author.displayAvatarURL)
+          .setDescription(`[${musik._music.queue[0].info.title}](${musik._music.queue[0].info.loaderUrl})\n`
+          + `Hinzugefügt von: ${musik._music.queue[0].requester}\n`
+          + `Stand: \`(${musik._formatSecs(Math.floor(musik._music.disp.time / 1000))}/${musik._formatSecs(musik._music.queue[0].info.length_seconds)})\`\n`))
+          .then(mes => mes.delete(30000));
+        bot.info(`[${msg.guild.id}] Song skipped.`);
+        musik._music.disp.end('skip');
       }
-      const response = `-- **${musik._music.queue[0].info.title}**`;
-      bot.info(`[${msg.guild.id}] Song skipped.`);
-      musik._music.disp.end('skip');
-      msg.channel.sendMessage(response)
-        .then((mes) => mes.delete(30000));
     }
   }
 };
