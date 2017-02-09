24b00e030b1a9a7ede6e160bdaedc6218f0194a2
Pascal <wermail2002-kap@yahoo.de>
2017-02-09 16:06:50 +0100

    rewrote stop command

--- commands/Musik/stop.js
+++ commands/Musik/stop.js
@@ -3,17 +3,23 @@ exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unuse
     if (!bot.internal.musik.get(msg.guild.id)) {
       msg.channel.send('Zur Zeit wird leider nichts gespielt.')
         .then(mes => mes.delete(30000));
+    } else if (!msg.guild.member(bot.user).voiceChannel) {
+      msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
+        .then((mes) => mes.delete(5000));
+    } else if (!bot.internal.music.channel(bot, msg)) {
+      msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
+        .then((mes) => mes.delete(5000));
     } else {
       const musik = bot.internal.musik.get(msg.guild.id);
-      if (!msg.guild.member(bot.user).voiceChannel) {
-        msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
-          .then((mes) => mes.delete(5000));
-      } else if (!bot.internal.music.channel(bot, msg)) {
-
-        msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
-          .then((mes) => mes.delete(5000));
+      if (musik._music.queue.length === 0) {
+        msg.channel.sendMessage('Die Queue ist leer.');
       } else {
-        msg.channel.send(musik.stop())
+        const response = `Leere die Queue (**${musik._music.queue.length}** Songs) und beende die Wiedergabe.`;
+        bot.info(`[${msg.guild.id}] Stopped playing through command.`);
+        musik._music.queue = musik._music.queue.slice(musik._music.queue.length - 1);
+        if (musik._music.disp) musik._music.disp.end('stop');
+        bot.setTimeout(musik._leaveChannel.bind(musik), 2000);
+        msg.channel.send(response)
           .then((mes) => mes.delete(5000));
       }
     }
