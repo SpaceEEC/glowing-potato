5e3aa87fb48816de868f4bc14efaeadeb8037ad5
Pascal <wermail2002-kap@yahoo.de>
2017-02-09 15:14:01 +0100

    rewrote playlist shuffle command

--- commands/Musik/shuffle.js
+++ commands/Musik/shuffle.js
@@ -1,24 +1,34 @@
 exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unused-vars
-  if (msg.permlvl >= 5
-    || ((!msg.conf.musicrole || (msg.conf.musicrole && msg.member.roles.has(msg.conf.musicrole)))
-      && (!msg.conf.musicchannel || (msg.conf.musicchannel && msg.channel.id === msg.conf.musicchannel)))) {
+  if (bot.internal.music.perms(msg)) {
     if (!bot.internal.musik.get(msg.guild.id)) {
       msg.channel.send('Zur Zeit wird leider nichts gespielt.')
         .then(mes => mes.delete(30000));
+    } else if (!msg.guild.member(bot.user).voiceChannel) {
+      msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
+        .then((mes) => mes.delete(5000));
+    } else if (!bot.internal.music.channel(bot, msg)) {
+      msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
+        .then((mes) => mes.delete(5000));
     } else {
       const musik = bot.internal.musik.get(msg.guild.id);
-      if (!msg.guild.member(bot.user).voiceChannel) {
-        msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
-          .then((mes) => mes.delete(5000));
-      } else if (msg.guild.member(bot.user).voiceChannel
-        && (msg.guild.member(bot.user).voiceChannel.id
-          !== msg.member.voiceChannel.id)) {
-        msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
-          .then((mes) => mes.delete(5000));
-      } else {
-        msg.channel.send(musik.shuffle())
-          .then((mes) => mes.delete(5000));
+      if (musik._music.queue.length < 3) {
+        msg.channel.sendMessage('Es lohnt sich nicht so wenig Lieder zu mischen.');
       }
+      const array = musik._music.queue.slice(1);
+      let currentIndex = array.length;
+      let temporaryValue;
+      let randomIndex;
+      while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      array.splice(0, 0, musik._music.queue[0]);
+      musik._music.queue = array;
+      msg.channel.send('Die Warteschlange wurde gemischt.')
+        .then((mes) => mes.delete(5000));
     }
   }
 };
