383d0385572db2e606ce5933c4a576264570e1fe
SpaceEEC <spaceeec@yahoo.com>
2017-02-10 01:46:18 +0100

    improved prune command

--- commands/Modstuff/prune.js
+++ commands/Modstuff/prune.js
@@ -1,27 +1,26 @@
 exports.run = (bot, msg, params = []) => {
-  if (!msg.member.hasPermission('MANAGE_MESSAGES')) {
-    return msg.channel.sendMessage('Du darfst keine Chatnachrichten löschen, also darf ich das leider nicht für dich tun.'); // eslint-disable-line
-  }
-  if (!msg.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')) {
-    return msg.channel.sendMessage('Ich darf keine Nachrichten löschen.');
-  }
-  if (params[0] % 1 !== 0) {
-    return msg.channel.sendEmbed(new bot.methods.Embed()
+  if (!msg.channel.permissionsFor(msg.member).hasPermission('MANAGE_MESSAGES')) {
+    msg.channel.sendMessage('Du darfst keine Chatnachrichten löschen, also darf ich das leider nicht für dich tun.'); // eslint-disable-line
+  } else if (!msg.channel.permissionsFor(bot.user).hasPermission('MANAGE_MESSAGES')) {
+    msg.channel.sendMessage('Ich darf keine Nachrichten löschen.');
+  } else if (params[0] % 1 !== 0) {
+    msg.channel.sendEmbed(new bot.methods.Embed()
       .setColor(0xff0000)
       .addField('Fehlerhafter Parameter',
       `\`${params[0]}\` ist keine Zahl!`)
       .setFooter(`${msg.author.username}: ${msg.content}`,
       bot.user.avatarURL)
     );
+  } else {
+    msg.channel.bulkDelete(parseInt(params[0]) + 1);
+    msg.channel.sendEmbed(new bot.methods.Embed()
+      .setColor(0x00ff08)
+      .addField('Erfolgreich',
+      `${parseInt(params[0]) + 1} Nachrichten gelöscht!`)
+      .setFooter(`${msg.author.username}: ${msg.content}`,
+      msg.author.avatarURL)
+    ).then((mes) => mes.delete(5000));
   }
-  msg.channel.bulkDelete(parseInt(params[0]) + 1);
-  return msg.channel.sendEmbed(new bot.methods.Embed()
-    .setColor(0x00ff08)
-    .addField('Erfolgreich',
-    `${parseInt(params[0]) + 1} Nachrichten gelöscht!`)
-    .setFooter(`${msg.author.username}: ${msg.content}`,
-    msg.author.avatarURL)
-  ).then((mes) => mes.delete(5000));
 };
 
 
