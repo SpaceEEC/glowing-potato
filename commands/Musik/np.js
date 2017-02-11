443eef9572a757452ed90737cbbe0eae683cc006
SpaceEEC <spaceeec@yahoo.com>
2017-02-11 20:19:07 +0100

    tweaked np command

--- commands/Musik/np.js
+++ commands/Musik/np.js
@@ -1,4 +1,4 @@
-exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unused-vars
+exports.run = async (bot, msg, params = []) => {
   if (!bot.internal.musik.get(msg.guild.id)) {
     msg.channel.send('Zur Zeit wird leider nichts gespielt.')
       .then(mes => mes.delete(30000));
@@ -7,11 +7,12 @@ exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unuse
     if (!musik._music.queue[0]) {
       msg.channel.send('Die Queue ist leer.').then(mes => mes.delete(30000));
     } else {
-      msg.channel.sendEmbed(new bot.methods.Embed().setColor(0x0800ff).setThumbnail(musik._music.queue[0].info.iurl)
-        .setTitle(musik._music.playing ? '**Wird gerade gespielt:**' : '**Momentan pausiert:**')
-        .setDescription(`[${musik._music.queue[0].info.title}](${musik._music.queue[0].info.loaderUrl})\n`
-        + `Hinzugefügt von: ${musik._music.queue[0].requester}\n`
-        + `Stand: \`(${musik._formatSecs(Math.floor(musik._music.disp.time / 1000))}/${musik._formatSecs(musik._music.queue[0].info.length_seconds)})\`\n`))
+      if (isNaN(params[0]) || !musik._music.queue[0]) params[0] = 0;
+      msg.channel.sendEmbed(new bot.methods.Embed().setColor(0x0800ff).setThumbnail(musik._music.queue[params[0]].info.iurl)
+        .setTitle(params[0] ? `**An Position: \`${params[0]}\`**` : musik._music.playing ? '**Wird gerade gespielt:**' : '**Momentan pausiert:**')
+        .setDescription(`[${musik._music.queue[params[0]].info.title}](${musik._music.queue[params[0]].info.loaderUrl})\n`
+        + `Hinzugefügt von: ${musik._music.queue[params[0]].requester}\n`
+        + `Stand: \`(${musik._formatSecs(Math.floor(musik._music.disp.time / 1000))}/${musik._formatSecs(musik._music.queue[params[0]].info.length_seconds)})\`\n`))
         .then(mes => mes.delete(30000));
     }
   }
