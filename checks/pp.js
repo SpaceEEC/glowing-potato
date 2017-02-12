bbfa6c5dfd21dc8871b2432b8ede40e084037808
Pascal <wermail2002-kap@yahoo.de>
2017-02-12 03:42:30 +0100

    removed unnecessary function

--- checks/pp.js
+++ checks/pp.js
@@ -1,51 +1,33 @@
 exports.run = (bot, msg, cmd) => new Promise((resolve, reject) => {
   let permlvl = 1;
-  try {
-    msg.guild.fetchMember(msg.author).then((member) => {
-      const modrole = msg.guild.roles.get(msg.conf.modrole);
-      if (modrole && member.roles.has(modrole.id)) {
-        permlvl = 5;
+  msg.guild.fetchMember(msg.author).then((member) => {
+    const modrole = msg.guild.roles.get(msg.conf.modrole);
+    if (modrole && member.roles.has(modrole.id)) {
+      permlvl = 5;
+    }
+    const adminrole = msg.guild.roles.get(msg.conf.adminrole);
+    if (adminrole && member.roles.has(adminrole.id)) {
+      permlvl = 10;
+    }
+    /* const users = bot.confs.get(msg.guild.id).users;
+    if (users[msg.author.id]) {
+      if (permlvl < users[msg.author.id].pp) {
+        permlvl = users[msg.author.id].pp;
       }
-      const adminrole = msg.guild.roles.get(msg.conf.adminrole);
-      if (adminrole && member.roles.has(adminrole.id)) {
-        permlvl = 10;
-      }
-      /* const users = bot.confs.get(msg.guild.id).users;
-      if (users[msg.author.id]) {
-        if (permlvl < users[msg.author.id].pp) {
-          permlvl = users[msg.author.id].pp;
-        }
-      } */
-      if (member === msg.guild.owner) {
-        permlvl = 11;
-      }
-      if (msg.author.id === bot.config.ownerID) {
-        permlvl = 12;
-      }
-      msg.permlvl = permlvl;
-      check(bot, permlvl, cmd)
-        .then(() => {
-          resolve(permlvl);
-        })
-        .catch((e) => {
-          reject(e);
-        });
-    });
-  } catch (e) {
-    reject(e);
-  }
+    } */
+    if (member === msg.guild.owner) {
+      permlvl = 11;
+    }
+    if (msg.author.id === bot.config.ownerID) {
+      permlvl = 12;
+    }
+    msg.permlvl = permlvl;
+    if (permlvl >= cmd.conf.permLevel) resolve();
+    else reject(`Du darfst den Befehl \`${cmd.help.name}\` leider nicht verwenden.`);
+  });
 });
+
 exports.conf = {
   spamProtection: false,
   enabled: true,
 };
-
-
-const check = (bot, permlvl, cmd) => new Promise((resolve, reject) => {
-  if (permlvl >= cmd.conf.permLevel) {
-    resolve();
-  } else {
-    reject(`Du darfst den Befehl \`${cmd.help.name}\` leider nicht verwenden.`);
-  }
-});
-
