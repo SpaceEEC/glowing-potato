0ba16cb143a8cbd33d8709ec150990d72ce858a0
Pascal <wermail2002-kap@yahoo.de>
2017-02-12 17:53:13 +0100

    You can not .join() strings.

--- commands/tags/tag-list.js
+++ commands/tags/tag-list.js
@@ -10,12 +10,12 @@ exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unuse
     });
   if (!alle[0]) {
     if (users[0]) {
-      alle = 'Dir gehören alle Tags auf diesem Server, du könntest andere dazu anstiften auch mal Tags zu erstellen.';
+      alle = ['Dir gehören alle Tags auf diesem Server, du könntest andere dazu anstiften auch mal Tags zu erstellen.'];
     } else {
-      alle = 'Es existieren keine Tags auf diesem Server, erstell doch welche!';
+      alle = ['Es existieren keine Tags auf diesem Server, erstell doch welche!'];
     }
   }
-  if (!users[0]) users = 'Du hast keine Tags, erstell\' doch welche!';
+  if (!users[0]) users = ['Du hast keine Tags, erstell\' doch welche!'];
   msg.channel.sendEmbed(new bot.methods.Embed()
     .setColor(0x3498db)
     .setTimestamp()
