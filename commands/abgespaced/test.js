51d07bd36377533611e4875c0a05659d3809a732
SpaceEEC <spaceeec@yahoo.com>
2017-02-18 18:13:43 +0100

    test to class

--- commands/abgespaced/test.js
+++ commands/abgespaced/test.js
@@ -1,58 +1,53 @@
-exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unused-vars
-  const cmd = new Testcommand(bot);
-  await cmd.run(msg, params);
-};
-
-class Testcommand {
+module.exports = class Test {
   constructor(bot) {
     this.bot = bot;
   }
-  async run(msg, params) {
-    this.test();
+
+
+  async run(msg, params) { // eslint-disable-line no-unused-vars
+    msg.channel.sendEmbed(this.nichts(this.bot, msg));
   }
 
-  async test() {
-    return new Promise(resolve => {
-      throw new Error('derp');
-    });
-  }
-}
 
-
-function nichts(bot, msg) { // eslint-disable-line no-unused-vars
-  return {
-    color: 0xFFFF00,
-    author: {
-      icon: bot.user.avatarURL,
-      name: bot.user.username,
-    },
-    fields: [
-      {
-        name: '¯\_(ツ)_/¯', // eslint-disable-line no-useless-escape
-        value: 'Dieser Befehl macht gerade nichts.\nVersuche es doch später erneut.',
+  nichts(bot, msg) { // eslint-disable-line no-unused-vars
+    return {
+      color: 0xFFFF00,
+      author: {
+        icon: bot.user.avatarURL,
+        name: bot.user.username,
       },
-    ],
-    thumbnail: { url: bot.user.avatarURL },
-    timestamp: new Date(),
-    footer: {
-      icon_url: msg.author.avatarURL,
-      text: msg.content,
-    },
-  };
-}
+      fields: [
+        {
+          name: '¯\_(ツ)_/¯', // eslint-disable-line no-useless-escape
+          value: 'Dieser Befehl macht gerade nichts.\nVersuche es doch später erneut.',
+        },
+      ],
+      thumbnail: { url: bot.user.avatarURL },
+      timestamp: new Date(),
+      footer: {
+        icon_url: msg.author.avatarURL,
+        text: msg.content,
+      },
+    };
+  }
 
 
-exports.conf = {
-  spamProtection: false,
-  enabled: true,
-  aliases: ['bakaero'],
-  permLevel: 0,
-};
+  static get conf() {
+    return {
+      spamProtection: false,
+      enabled: true,
+      aliases: ['bakaero'],
+      permLevel: 0,
+    };
+  }
 
 
-exports.help = {
-  name: 'test',
-  shortdescription: 'Oh shit',
-  description: 'Ein Testbefehl, welcher das macht wozu er gerade bestimmt ist.',
-  usage: '$conf.prefixtest ()/[]/""',
+  static get help() {
+    return {
+      name: 'test',
+      shortdescription: 'Oh shit',
+      description: 'Ein Testbefehl, welcher das macht wozu er gerade bestimmt ist.',
+      usage: '$conf.prefixtest ()/[]/""',
+    };
+  }
 };
