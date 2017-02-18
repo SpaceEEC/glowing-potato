432f38b3181bc3bd75656e7cc9ef218da5313da7
SpaceEEC <spaceeec@yahoo.com>
2017-02-18 18:13:39 +0100

    merged eval to class

--- commands/abgespaced/eval.js
+++ commands/abgespaced/eval.js
@@ -1,19 +1,26 @@
-exports.run = async (bot, msg, params = []) => {
-  if (msg.author.id === bot.config.ownerID) {
-    const time = +new Date;
-    try {
-      const code = params.join(' ');
-      let evaled;
-      if (msg.cmd === 'async') evaled = eval(`(async(bot,msg,params=[])=>{${code}})(bot,msg,params);`);
-      else evaled = eval(code);
-      if (evaled instanceof Promise) evaled = await evaled;
-      const response_typeof = typeof evaled;
-      if (typeof evaled !== 'string') { evaled = bot.inspect(evaled, false, 0); }
-      if (evaled.includes(bot.token)) {
-        msg.channel.sendMessage('Was willst du damit anstellen? 👀.');
-        return;
-      }
-      msg.channel.sendMessage(`\`code:\`
+module.exports = class Eval {
+  constructor(bot) {
+    this.bot = bot;
+  }
+
+
+  async run(msg, params) {
+    if (msg.author.id === this.bot.config.ownerID) {
+      const time = +new Date;
+      const bot = this.bot;
+      try {
+        const code = params.join(' ');
+        let evaled;
+        if (msg.cmd === 'async') evaled = eval(`(async(bot,msg,params=[])=>{${code}})(bot,msg,params);`);
+        else evaled = eval(code);
+        if (evaled instanceof Promise) evaled = await evaled;
+        const response_typeof = typeof evaled;
+        if (typeof evaled !== 'string') { evaled = bot.inspect(evaled, false, 0); }
+        if (evaled.includes(bot.token)) {
+          msg.channel.sendMessage('Was willst du damit anstellen? 👀.');
+          return;
+        }
+        msg.channel.sendMessage(`\`code:\`
 \`\`\`js\n${code ? code.split(`\``).join(`´`) : 'falsy'}\`\`\`
 \`evaled\\returned:\`
 \`\`\`js\n${evaled ? evaled.split(`\``).join(`´`) : 'falsy'}\`\`\`
@@ -27,31 +34,33 @@ Ausführungszeitraum: \`${new Date().getTime() - time}\`ms`).catch((e) => {
 ${e.stack ? e.stack : e}
 \`\`\``);
 });
-    } catch (e) {
-      msg.channel.sendMessage(`\`E-ROHR\`
+      } catch (e) {
+        msg.channel.sendMessage(`\`E-ROHR\`
 \`\`\`js
 ${e}${e.response && e.response.res && e.response.res.text ? `\n${e.response.res.text}` : ''}
 \`\`\`
 
 Versuchungszeitraum: \`${new Date().getTime() - time}\`ms`);
+      }
+    } else {
+      msg.channel.sendMessage('Du kannst den `eval` Befehl leider nicht verwenden.');
     }
-  } else {
-    msg.channel.sendMessage('Du kannst den `eval` Befehl leider nicht verwenden.');
   }
-};
 
-
-exports.conf = {
-  spamProtection: false,
-  enabled: true,
-  aliases: ['async'],
-  permLevel: 12,
-};
-
-
-exports.help = {
-  name: 'eval',
-  shortdescription: 'eval()',
-  description: 'Führt code in JavaScript aus.',
-  usage: '$conf.prefixeval [👀]',
+  static get conf() {
+    return {
+      spamProtection: false,
+      enabled: true,
+      aliases: ['async'],
+      permLevel: 12,
+    };
+  }
+  static get help() {
+    return {
+      name: 'eval',
+      shortdescription: 'eval()',
+      description: 'Führt code in JavaScript aus.',
+      usage: '$conf.prefixeval [👀]',
+    };
+  }
 };
