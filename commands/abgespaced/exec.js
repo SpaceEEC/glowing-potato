e7dd916f4f1af29870faf5296590984fd08103b7
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-18 18:18:36 +0100

    exec command

--- commands/abgespaced/exec.js
+++ commands/abgespaced/exec.js
@@ -1,54 +1,65 @@
-exports.run = async (bot, msg, params = []) => {
-  if (msg.author.id === bot.config.ownerID) {
-    const mes = await msg.channel.sendEmbed(new bot.methods.Embed()
-      .setColor(0x0800ff)
-      .setDescription('Befehl wird ausgeführt...'));
-    const exec = require('child_process');
-    const time = +new Date;
-    return exec.exec(params.join(' '), (error, stdout, stderr) => {
-      if (error) {
-        return mes.edit(`\`EXEC\`\n
+module.exports = class Exec {
+  constructor(bot) {
+    this.bot = bot;
+  }
+
+  async run(msg, params) {
+    if (msg.author.id === this.bot.config.ownerID) {
+      const mes = await msg.channel.sendEmbed(new this.bot.methods.Embed()
+        .setColor(0x0800ff)
+        .setDescription('Befehl wird ausgeführt...'));
+      const exec = require('child_process');
+      const time = +new Date;
+      return exec.exec(params.join(' '), (error, stdout, stderr) => {
+        if (error) {
+          return mes.edit(`\`EXEC\`\n
 \`\`\`xl\n${params.join(' ')}\n\`\`\`
 ${stdout ? `\`STDOUT\`\n\`\`\`xl\n${stdout}\`\`\`` : ''}
 ${error.stack ? `\`E-ROHR\`\n\`\`\`js\n${error.stack}\n\`\`\`` : ''}
 ${error.code ? `Error Code: ${error.code}` : ''}
 ${error.signal ? `Signal received: ${error.signal}` : ''}
 `, {
-  embed: new bot.methods.Embed()
-              .setColor(0xffff00)
-              .setDescription(`Befehl wurde mit Fehler(n) in \`${new Date().getTime() - time}\`ms ausgeführt.`),
+  embed: new this.bot.methods.Embed()
+                .setColor(0xffff00)
+                .setDescription(`Befehl wurde mit Fehler(n) in \`${new Date().getTime() - time}\`ms ausgeführt.`),
 });
-      } else {
-        return mes.edit(`\`EXEC\`
+        } else {
+          return mes.edit(`\`EXEC\`
 \`\`\`xl\n${params.join(' ')}\n\`\`\`
 ${stdout ? `\`STDOUT\`\n\`\`\`xl\n${stdout}\`\`\`` : ''}
 ${stderr ? `\`STERR\`\n\`\`\`xl\n${stderr}\`\`\`` : ''}
 `, {
-  embed: new bot.methods.Embed()
-              .setColor(0x00ff08)
-              .setDescription(`Befehl wurde erfolgreich in \`${new Date().getTime() - time}\`ms ausgeführt.`),
+  embed: new this.bot.methods.Embed()
+                .setColor(0x00ff08)
+                .setDescription(`Befehl wurde erfolgreich in \`${new Date().getTime() - time}\`ms ausgeführt.`),
 });
-      }
-    });
-  } else {
-    return msg.channel.sendEmbed(new bot.methods.Embed()
-      .setColor(0xff0000)
-      .setDescription('Du solltest nicht einmal hier angekommen sein.'));
+        }
+      });
+    } else {
+      return msg.channel.sendEmbed(new this.bot.methods.Embed()
+        .setColor(0xff0000)
+        .setDescription('Du solltest nicht einmal hier angekommen sein.'));
+    }
   }
-};
 
 
-exports.conf = {
-  spamProtection: false,
-  enabled: true,
-  aliases: [],
-  permLevel: 12,
-};
+  static get conf() {
+    return {
+      spamProtection: false,
+      enabled: true,
+      aliases: [],
+      permLevel: 12,
+
+    };
+  }
 
 
-exports.help = {
-  name: 'exec',
-  shortdescription: 'Bash',
-  description: 'Führt den angegebenen Befehl als Child_Process aus.',
-  usage: '$conf.prefixexec [Befehl Argumente bla]',
+  static get help() {
+    return {
+      name: 'exec',
+      shortdescription: 'Bash',
+      description: 'Führt den angegebenen Befehl als Child_Process aus.',
+      usage: '$conf.prefixexec [Befehl Argumente bla]',
+    };
+  }
 };
