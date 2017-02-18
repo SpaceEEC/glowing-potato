e408b552d3b7a2d6192e2ec5c5b14a5032a94afe
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-18 19:26:54 +0100

    prunemembers

--- commands/Adminstuff/prunemembers.js
+++ commands/Adminstuff/prunemembers.js
@@ -1,40 +1,51 @@
-exports.run = async (bot, msg, params = []) => { // eslint-disable-line consistent-return
-  let days;
-  if (!params[0]) {
-    const mes = await msg.channel.sendMessage('Wir kicken heute also inaktive Member?\nVon wie vielen Tagen der Inaktivität sprechen wir hier denn?');
+module.exports = class Prune {
+  constructor(bot) {
+    this.bot = bot;
+  }
+
+
+  async run(msg, params) {
+    let days;
+    if (!params[0]) {
+      const mes = await msg.channel.sendMessage('Wir kicken heute also inaktive Member?\nVon wie vielen Tagen der Inaktivität sprechen wir hier denn?');
+      const collected = (await mes.channel.awaitMessages(m => m.author.id === msg.author.id, { time: 30000, maxMatches: 1 })).first();
+      mes.delete();
+      if (!collected) return msg.delete();
+      days = parseInt(collected.content.split(' ')[0]);
+      if (!days) return msg.channel.sendMessage('Bei dieser Eingabe handelt es sich um keine gültige Zahl!');
+    }
+    const count = await msg.guild.pruneMembers(days, true);
+    const mes = await msg.channel.sendMessage(`Das wären dann ${count} Member welche gekickt werden sollen.\nIst das korrekt? (__j__a/__y__es oder __n__ein/__n__o)`);
     const collected = (await mes.channel.awaitMessages(m => m.author.id === msg.author.id, { time: 30000, maxMatches: 1 })).first();
     mes.delete();
-    if (!collected) return msg.delete();
-    days = parseInt(collected.content.split(' ')[0]);
-    if (!days) return msg.channel.sendMessage('Bei dieser Eingabe handelt es sich um keine gültige Zahl!');
+    if (!collected) return mes.delete();
+    if (['ja', 'yes', 'j', 'y'].includes(collected.content.split(' ')[0])) {
+      const kicked = await msg.guild.pruneMembers(days);
+      return msg.channel.sendMessage(`Es wurden erfolgreich ${kicked} Member gekickt!`);
+    } else if (['nein', 'no', 'n'].includes(collected.content.split(' ')[0])) {
+      return msg.channel.sendMessage('Breche den Vorgang dann, wie gewünscht, ab.');
+    } else {
+      return msg.channel.sendMessage('Konnte diese Eingabe nicht eindeutig zuordnen, breche den Vorgang daher vorsichtshalber ab.');
+    }
   }
-  const count = await msg.guild.pruneMembers(days, true);
-  const mes = await msg.channel.sendMessage(`Das wären dann ${count} Member welche gekickt werden sollen.\nIst das korrekt? (__j__a/__y__es oder __n__ein/__n__o)`);
-  const collected = (await mes.channel.awaitMessages(m => m.author.id === msg.author.id, { time: 30000, maxMatches: 1 })).first();
-  mes.delete();
-  if (!collected) return mes.delete();
-  if (['ja', 'yes', 'j', 'y'].includes(collected.content.split(' ')[0])) {
-    const kicked = await msg.guild.pruneMembers(days);
-    msg.channel.sendMessage(`Es wurden erfolgreich ${kicked} Member gekickt!`);
-  } else if (['nein', 'no', 'n'].includes(collected.content.split(' ')[0])) {
-    msg.channel.sendMessage('Breche den Vorgang dann, wie gewünscht, ab.');
-  } else {
-    msg.channel.sendMessage('Konnte diese Eingabe nicht eindeutig zuordnen, breche den Vorgang daher vorsichtshalber ab.');
+
+
+  static get conf() {
+    return {
+      spamProtection: false,
+      enabled: true,
+      aliases: [],
+      permLevel: 11,
+    };
   }
-};
 
 
-exports.conf = {
-  spamProtection: false,
-  enabled: true,
-  aliases: [],
-  permLevel: 11,
-};
-
-
-exports.help = {
-  name: 'prunemembers',
-  shortdescription: 'Member kicken',
-  description: 'Kickt alle Member, welche keine Rolle haben und `n` Tage inaktiv waren.',
-  usage: '$conf.prefixprunemembers [Tage]',
+  static get help() {
+    return {
+      name: 'prunemembers',
+      shortdescription: 'Member kicken',
+      description: 'Kickt alle Member, welche keine Rolle haben und `n` Tage inaktiv waren.',
+      usage: '$conf.prefixprunemembers [Tage]',
+    };
+  }
 };
