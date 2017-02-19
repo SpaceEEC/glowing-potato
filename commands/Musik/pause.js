66606fc293373cd2c2be530ba2acda780cd88479
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-19 01:13:04 +0100

    pause (music)

--- commands/Musik/pause.js
+++ commands/Musik/pause.js
@@ -1,36 +1,48 @@
-exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unused-vars
-  if (bot.internal.music.perms(msg)) {
-    if (!bot.internal.musik.get(msg.guild.id)) {
-      msg.channel.send('Zur Zeit wird leider nichts gespielt.')
-        .then(mes => mes.delete(30000));
-    } else {
-      const musik = bot.internal.musik.get(msg.guild.id);
-      if (!msg.guild.member(bot.user).voiceChannel) {
-        msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
-          .then((mes) => mes.delete(5000));
-      } else if (!bot.internal.music.channel(bot, msg)) {
-        msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
-          .then((mes) => mes.delete(5000));
+module.exports = class Pause {
+  constructor(bot) {
+    this.bot = bot;
+  }
+
+
+  async run(msg, params = []) { // eslint-disable-line no-unused-vars
+    if (this.bot.internal.music.perms(msg)) {
+      if (!this.bot.internal.musik.get(msg.guild.id)) {
+        msg.channel.send('Zur Zeit wird leider nichts gespielt.')
+          .then(mes => mes.delete(30000));
       } else {
-        msg.channel.send(musik.toggleState(false) ? 'Fortgesetzt.' : 'Pausiert.')
-          .then((mes) => mes.delete(5000));
+        const musik = this.bot.internal.musik.get(msg.guild.id);
+        if (!msg.guild.member(this.bot.user).voiceChannel) {
+          msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
+            .then((mes) => mes.delete(5000));
+        } else if (!this.bot.internal.music.channel(this.bot, msg)) {
+          msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
+            .then((mes) => mes.delete(5000));
+        } else {
+          msg.channel.send(musik.toggleState(false) ? 'Fortgesetzt.' : 'Pausiert.')
+            .then((mes) => mes.delete(5000));
+        }
       }
     }
   }
-};
 
 
-exports.conf = {
-  spamProtection: false,
-  enabled: true,
-  aliases: [],
-  permLevel: 0,
-};
+  static get conf() {
+    return {
+      spamProtection: false,
+      enabled: true,
+      aliases: [],
+      permLevel: 0,
+      group: __dirname.split(require('path').sep).pop()
+    };
+  }
 
 
-exports.help = {
-  name: 'pause',
-  shortdescription: '',
-  description: 'Pausiert den aktuellen Song.',
-  usage: '$conf.prefixpause',
+  static get help() {
+    return {
+      name: 'pause',
+      shortdescription: '',
+      description: 'Pausiert den aktuellen Song.',
+      usage: '$conf.prefixpause',
+    };
+  }
 };
