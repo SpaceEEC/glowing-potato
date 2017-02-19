c33bdb834c2d6407b3ad8aa81255e4ea62b08894
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-19 01:12:45 +0100

    music

--- commands/Musik/music.js
+++ commands/Musik/music.js
@@ -1,30 +1,42 @@
-exports.run = async (bot, msg, params = []) => { // eslint-disable-line no-unused-vars
-  bot.commands.get('help').run(bot, msg, ['music']);
-};
+module.exports = class Music {
+  constructor(bot) {
+    this.bot = bot;
+  }
 
 
-exports.conf = {
-  spamProtection: false,
-  enabled: true,
-  aliases: [],
-  permLevel: 0,
-};
+  async run(msg, params = []) { // eslint-disable-line no-unused-vars
+    this.bot.commands.get('help').run(this.bot, msg, ['music']);
+  }
 
 
-exports.help = {
-  name: 'music',
-  description: 'Ermöglicht die Interaktion mit dem Musicfeature des Bots.\n'
-  + 'Für weitere Hilfe `$conf.prefixhelp [Befehl]` verwenden.',
-  shortdescription: '`$conf.prefixhelp music`',
-  usage: `\`$conf.prefixplay [ID/Url] (Anzahl)\` - Spielt Songs oder fügt diese hinzu.\n`
-  + `\`$conf.prefixsearch (-N) [Suchgebriffe]\` - Ermöglicht die Suche nach Songs auf Youtube.\n`
-  + `\`$conf.prefixnp\` - Zeigt den aktuell gespielten Song an.\n`
-  + `\`$conf.prefixqueue (Seite)\` - Zeigt die aktuelle Warteschlange an.\n`
-  + `\`$conf.prefixskip\` - Überspringt den aktuellen Song.\n`
-  + `\`$conf.prefixpause\` - Pausiert den aktuellen Song.\n`
-  + `\`$conf.prefixresume\` - Setzt den aktuell pausierten Song fort.\n`
-  + `\`$conf.prefixstop\` - Beendet die Wiedergabe und löscht die Queue.\n`
-  + `\`$conf.prefixvolume (0-200)\` - Setzt die Lautstärke von \`0\` bis \`200\`. (Standardwert beträgt 20.)\n`
-  + `\`$conf.prefixshuffle\` - Mischt einmal kräftig durch die Warteschlange durch.\n`
-  + `\`$conf.prefixloop (an|aus)\` - Aktiviert/Deaktiviert das Wiederholen, des aktuellen Songs.\n`,
+  static get conf() {
+    return {
+      spamProtection: false,
+      enabled: true,
+      aliases: [],
+      permLevel: 0,
+      group: __dirname.split(require('path').sep).pop()
+    };
+  }
+
+
+  static get help() {
+    return {
+      name: 'music',
+      description: 'Ermöglicht die Interaktion mit dem Musicfeature des Bots.\n'
+      + 'Für weitere Hilfe `$conf.prefixhelp [Befehl]` verwenden.',
+      shortdescription: '`$conf.prefixhelp music`',
+      usage: `\`$conf.prefixplay [ID/Url] (Anzahl)\` - Spielt Songs oder fügt diese hinzu.\n`
+      + `\`$conf.prefixsearch (-N) [Suchgebriffe]\` - Ermöglicht die Suche nach Songs auf Youtube.\n`
+      + `\`$conf.prefixnp\` - Zeigt den aktuell gespielten Song an.\n`
+      + `\`$conf.prefixqueue (Seite)\` - Zeigt die aktuelle Warteschlange an.\n`
+      + `\`$conf.prefixskip\` - Überspringt den aktuellen Song.\n`
+      + `\`$conf.prefixpause\` - Pausiert den aktuellen Song.\n`
+      + `\`$conf.prefixresume\` - Setzt den aktuell pausierten Song fort.\n`
+      + `\`$conf.prefixstop\` - Beendet die Wiedergabe und löscht die Queue.\n`
+      + `\`$conf.prefixvolume (0-200)\` - Setzt die Lautstärke von \`0\` bis \`200\`. (Standardwert beträgt 20.)\n`
+      + `\`$conf.prefixshuffle\` - Mischt einmal kräftig durch die Warteschlange durch.\n`
+      + `\`$conf.prefixloop (an|aus)\` - Aktiviert/Deaktiviert das Wiederholen, des aktuellen Songs.\n`,
+    };
+  }
 };
