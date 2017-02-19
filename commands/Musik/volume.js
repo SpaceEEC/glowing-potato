b6820b7f186c256f1a8859d3afad1e605e99a7cd
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-19 01:38:50 +0100

    volume

--- commands/Musik/volume.js
+++ commands/Musik/volume.js
@@ -1,43 +1,55 @@
-exports.run = async (bot, msg, params = []) => {
-  if (!bot.internal.musik.get(msg.guild.id)) {
-    msg.channel.send('Zur Zeit wird leider nichts gespielt.')
-      .then(mes => mes.delete(30000));
-  } else {
-    const musik = bot.internal.musik.get(msg.guild.id);
-    if (bot.internal.music.perms(msg)) {
-      if (!msg.guild.member(bot.user).voiceChannel) {
-        msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
-          .then((mes) => mes.delete(5000));
-      } else if (!bot.internal.music.channel(bot, msg)) {
-        msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
-          .then((mes) => mes.delete(5000));
-      } else if (params[0] % 1 === 0) {
-        if (parseInt(params[0]) > 200 || parseInt(params[0]) < 0) {
-          msg.channel.sendMessage('Bitte nur Zahlen von `0` bis `200` eingeben.');
+module.exports = class Volume {
+  constructor(bot) {
+    this.bot = bot;
+  }
+
+
+  async run(msg, params = []) {
+    if (!this.bot.internal.musik.get(msg.guild.id)) {
+      msg.channel.send('Zur Zeit wird leider nichts gespielt.')
+        .then(mes => mes.delete(30000));
+    } else {
+      const musik = this.bot.internal.musik.get(msg.guild.id);
+      if (this.bot.internal.music.perms(msg)) {
+        if (!msg.guild.member(this.bot.user).voiceChannel) {
+          msg.channel.sendMessage('Zur Zeit spiele ich leider nichts.')
+            .then((mes) => mes.delete(5000));
+        } else if (!this.bot.internal.music.channel(this.bot, msg)) {
+          msg.channel.sendMessage('Für diesen Befehl müssen wir uns leider beide im selben Channel befinden.')
+            .then((mes) => mes.delete(5000));
+        } else if (params[0] % 1 === 0) {
+          if (parseInt(params[0]) > 200 || parseInt(params[0]) < 0) {
+            msg.channel.sendMessage('Bitte nur Zahlen von `0` bis `200` eingeben.');
+          } else {
+            msg.channel.send(musik.volume(Math.round(params[0] / 10) / 10));
+          }
         } else {
-          msg.channel.send(musik.volume(Math.round(params[0] / 10) / 10));
+          msg.channel.send(musik.volume('get'));
         }
       } else {
         msg.channel.send(musik.volume('get'));
       }
-    } else {
-      msg.channel.send(musik.volume('get'));
     }
   }
-};
 
 
-exports.conf = {
-  spamProtection: false,
-  enabled: true,
-  aliases: [],
-  permLevel: 0,
-};
+  static get conf() {
+    return {
+      spamProtection: false,
+      enabled: true,
+      aliases: [],
+      permLevel: 0,
+      group: __dirname.split(require('path').sep).pop()
+    };
+  }
 
 
-exports.help = {
-  name: 'volume',
-  shortdescription: '',
-  description: 'Legt die Lautstärke fest, oder zeigt dies an. (Standardwert ist `20`)',
-  usage: '$conf.prefixvolume (0-200)',
+  static get help() {
+    return {
+      name: 'volume',
+      shortdescription: '',
+      description: 'Legt die Lautstärke fest, oder zeigt dies an. (Standardwert ist `20`)',
+      usage: '$conf.prefixvolume (0-200)',
+    };
+  }
 };
