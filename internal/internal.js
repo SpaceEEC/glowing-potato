91f6d03b8cce10803dec0fafc5a1b179a385faed
SpaceEEC <spaceeec@yahoo.com>
2017-02-23 02:56:38 +0100

    refactored internal.js - .slice instead of .substr - used async instead of promises everywhere

--- internal/internal.js
+++ internal/internal.js
@@ -1,29 +1,25 @@
 const fs = require('fs-extra-promise');
+const { sep } = require('path');
 
 exports.init = async (bot) => {
-  const modules = [];
   bot.config = {};
   bot.db = require('sqlite');
-  await bot.db.open('./var/db.sqlite');
+  await bot.db.open(`.${sep}var${sep}db.sqlite`);
   const stuff = await bot.db.get('SELECT * FROM config');
   for (let key in stuff) {
     bot.config[key] = stuff[key];
   }
-  const files = fs.readdirSync('./internal/');
-  files.forEach((f) => {
+  const files = await fs.readdirAsync(`.${sep}internal`);
+  for (const file of files) {
+    if (file === 'internal.js') continue;
     try {
-      if (f !== 'internal.js') {
-        delete require.cache[require.resolve(`./${f}`)];
-        bot.internal[f.substring(0, f.length - 3)] = require(`./${f}`);
-        modules.push(f.substring(0, f.length - 3));
-        if (bot.internal[f.substring(0, f.length - 3)].init) {
-          bot.internal[f.substring(0, f.length - 3)].init(bot).catch(e => bot.err(`${e}\n${e.stack}`));
-        }
+      delete require.cache[require.resolve(`./${file}`)];
+      bot.internal[file.slice(0, -3)] = require(`./${file}`);
+      if (bot.internal[file.slice(0, -3)].init) {
+        bot.internal[file.slice(0, -3)].init(bot).catch(e => bot.err(`${e}\n${e.stack}`));
       }
-    } catch (e) {
-      throw new Error(e.stack ? e.stack : e);
-    }
-  });
+    } catch (e) { bot.err(e.stack ? e.stack : e); }
+  }
 };
 
 exports.setGame = async (bot, game) => {
@@ -32,12 +28,7 @@ exports.setGame = async (bot, game) => {
   return bot.user.presence.game.name ? bot.user.presence.game.name : 'null';
 };
 
-exports.reload = (bot, name, file) => new Promise((resolve, reject) => {
-  try {
-    delete require.cache[require.resolve(`./${file}.js`)];
-    bot.internal[name] = require(`./${file}.js`);
-    resolve();
-  } catch (e) {
-    reject(e);
-  }
-});
+exports.reload = (bot, name, file) => {
+  delete require.cache[require.resolve(`./${file}.js`)];
+  bot.internal[name] = require(`./${file}.js`);
+};
