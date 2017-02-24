4f56e930adf4984ec09a24539eff86c65a4831b6
SpaceEEC <spaceeec@yahoo.com>
2017-02-24 22:06:07 +0100

    small tweaks for react.js

--- commands/Modstuff/react.js
+++ commands/Modstuff/react.js
@@ -11,19 +11,15 @@ module.exports = class React {
     if (!params[0]) {
       return msg.channel.sendEmbed(new this.bot.methods.Embed()
         .setColor(0xffee00)
-        .addField('Fehlender Parameter',
-        'Bitte ein Emoji und mindestens eine Nachrichten ID angeben.')
-        .setFooter(`${msg.author.username}: ${msg.content}`,
-        this.bot.user.avatarURL)
+        .addField('Fehlender Parameter', 'Bitte ein Emoji und mindestens eine Nachrichten ID angeben.')
+        .setFooter(`${msg.author.username}: ${msg.content}`, this.bot.user.avatarURL)
       );
     }
     if (!params[1]) {
       return msg.channel.sendEmbed(new this.bot.methods.Embed()
         .setColor(0xffee00)
-        .addField('Fehlender Parameter',
-        'Bitte mindestens eine Nachrichten ID angeben.')
-        .setFooter(`${msg.author.username}: ${msg.content}`,
-        this.bot.user.avatarURL)
+        .addField('Fehlender Parameter', 'Bitte mindestens eine Nachrichten ID angeben.')
+        .setFooter(`${msg.author.username}: ${msg.content}`, this.bot.user.avatarURL)
       );
     }
     const msgs = params.slice(1);
@@ -31,22 +27,17 @@ module.exports = class React {
       .exists('identifier', params[0].slice(2).slice(0, params[0].indexOf('>') - 2)) ? msg.guild.emojis
         .find('identifier', params[0].slice(2).slice(0, params[0].indexOf('>') - 2)) : params[0];
 
-    msgs.forEach((id) => {
+    for (const id of msgs) {
       msg.channel.fetchMessage(id).then(reactmsg => {
         reactmsg.react(emoji).catch(() => msg.channel.sendEmbed(new this.bot.methods.Embed()
-          .setColor(0xffee00)
-          .addField('Fehler',
-          `Ist das Emoji ${emoji} korrekt?`)
-          .setFooter(`${msg.author.username}: ${msg.content}`,
-          msg.author.avatarURL))
+          .setColor(0xffee00).addField('Fehler', `Ist das Emoji ${emoji} korrekt?`)
+          .setFooter(`${msg.author.username}: ${msg.content}`, msg.author.avatarURL))
         );
       }).catch(() => msg.channel.sendEmbed(new this.bot.methods.Embed()
-        .setColor(0xffee00)
-        .addField('Fehler', `Ist die ID ${id} korrekt?`)
-        .setFooter(`${msg.author.username}: ${msg.content}`,
-        msg.author.avatarURL))
+        .setColor(0xffee00).addField('Fehler', `Ist die ID ${id} korrekt?`)
+        .setFooter(`${msg.author.username}: ${msg.content}`, msg.author.avatarURL))
         );
-    });
+    }
     return msg.delete();
   }
 
