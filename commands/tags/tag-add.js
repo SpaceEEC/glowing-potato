7474193a366cf3ce247e3efa1b9c895c5b265e4c
SpaceEEC <spaceeec@yahoo.com>
2017-02-24 20:58:30 +0100

    tweaked tag-add.js

--- commands/tags/tag-add.js
+++ commands/tags/tag-add.js
@@ -7,50 +7,40 @@ module.exports = class TagAdd {
   }
 
 
-  async run(msg, params = []) { // eslint-disable-line consistent-return
+  async run(msg, params = []) {
     console.log(params);
     if (this.bot.commands.get('tag').conf.createLevel > msg.permlvl) return msg.channel.sendMessage('Du darfst leider keine Tags erstellen.');
     if (!params[0] || (params[0] && ['add', 'edit', 'remove', 'list'].includes(params[0])) || (params[0] && this.bot.internal.tags.has(`${msg.guild.id}|${params.join(' ')}`))) {
       const mes = await msg.channel.sendMessage('Bitte gib einen Namen für diesen neuen Tag an.\n\nDiese Anfrage wird bei Eingabe von `cancel`, einer fehlerhaften Eingabe, oder nach 30 Sekunden abgebrochen');
-      try {
-        const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000, errors: ['time'] });
-        if (collected.first().content === 'cancel') {
-          mes.delete();
-          return msg.channel.sendMessage('Breche die Anfrage, wie gewünscht, ab.');
-        } else if (['add', 'edit', 'remove', 'list'].includes(collected.first().content) || this.bot.internal.tags.has(`${msg.guild.id}|${collected.first().content}`)) {
-          return msg.channel.sendMessage('Dieser Tagname ist ungütlig oder bereits belegt.');
-        } else {
-          params[0] = collected.first().content;
-        }
-      } catch (e) {
+      const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000 });
+      if (!collected.size) {
         mes.delete();
         return msg.channel.sendMessage('Breche die Anfrage, wie durch die inaktivität gewünscht, ab.');
-      }
+      } else if (collected.first().content === 'cancel') {
+        mes.delete();
+        return msg.channel.sendMessage('Breche die Anfrage, wie gewünscht, ab.');
+      } else if (['add', 'edit', 'remove', 'list'].includes(collected.first().content) || this.bot.internal.tags.has(`${msg.guild.id}|${collected.first().content}`)) {
+        return msg.channel.sendMessage('Dieser Tagname ist ungütlig oder bereits belegt.');
+      } else { params[0] = collected.first().content; }
     }
     if (!params[1] || (params[1] && !this.bot.commands.get('tag').blacklist(msg, params.slice(1).join(' ')))) {
       const mes = await msg.channel.sendMessage('Gib bitte nun den Inhalt für den neuen Tag an.\n\nDiese Anfrage wird bei Eingabe von `cancel`, einer fehlerhaften Eingabe, oder nach 30 Sekunden abgebrochen');
-      try {
-        const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000, errors: ['time'] });
-        if (collected.first().content === 'cancel') {
-          mes.delete();
-          return msg.channel.sendMessage('Breche Anfrage, wie gewünscht, ab.');
-        } else if (!this.bot.commands.get('tag').blacklist(msg, collected.first().content)) {
-          return msg.channel.sendMessage(`Diese Antwort enthält einen Link, welcher nicht hotlinkbar scheint, verlinke das Bild über einen hotlinkbaren Hoster.\n\nFalls dieses Bild hotlinkbar ist kontaktiere \`${this.bot.config.owner}\``);
-        } else {
-          params[1] = collected.first().content;
-        }
-      } catch (e) {
+      const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1 });
+      if (!collected.size) {
         mes.delete();
         return msg.channel.sendMessage('Breche die Anfrage, wie durch die inaktivität gewünscht, ab.');
-      }
+      } else if (collected.first().content === 'cancel') {
+        mes.delete();
+        return msg.channel.sendMessage('Breche Anfrage, wie gewünscht, ab.');
+      } else if (!this.bot.commands.get('tag').blacklist(msg, collected.first().content)) {
+        return msg.channel.sendMessage(`Diese Antwort enthält einen Link, welcher nicht hotlinkbar scheint, verlinke das Bild über einen hotlinkbaren Hoster.\n\nFalls dieses Bild hotlinkbar ist kontaktiere \`${this.bot.config.owner}\``);
+      } else { params[1] = collected.first().content; }
     }
-    this.bot.internal.tag.add(this.bot, msg.guild.id, params[0].toLowerCase(), params.slice(1).join(' '), msg.author.id)
-      .then(() => {
-        msg.channel.sendMessage('Tag erfolgreich erstellt!');
-      })
+    return this.bot.internal.tag.add(this.bot, msg.guild.id, params[0].toLowerCase(), params.slice(1).join(' '), msg.author.id)
+      .then(() => msg.channel.sendMessage('Tag erfolgreich erstellt!'))
       .catch((e) => {
         this.bot.err(`[tag-add tag.add()] ${e}`);
-        msg.channel.sendMessage('Es ist ein interner Fehler beim Erstellen dieses Tags aufgetreten.\nDer Tag wurde vielleicht erstellt, bevor du es erneut zu erstellen versuchst, prüfe zunächst ob der Tag erstellt wurde.');
+        msg.channel.sendMessage(`Es ist ein interner Fehler beim Erstellen dieses Tags aufgetreten.\nDer Tag wurde vielleicht erstellt, bevor du es erneut zu erstellen versuchst, prüfe zunächst ob der Tag erstellt wurde.\n\nAndernfalls kontaktiere \`${this.bot.config.owner}\``);
       });
   }
 
