70c2f3a7b4af62a03f692fb98e4d2c001249ae15
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-24 23:19:46 +0100

    tweaked prune command

--- commands/Modstuff/prune.js
+++ commands/Modstuff/prune.js
@@ -7,23 +7,23 @@ module.exports = class Prune {
   }
 
 
-  async run(msg, params = []) { // eslint-disable-line consistent-return
+  async run(msg, params = []) {
     if (!msg.channel.permissionsFor(msg.member).hasPermission('MANAGE_MESSAGES') || !msg.channel.permissionsFor(this.bot.user).hasPermission('MANAGE_MESSAGES')) {
       return msg.channel.sendMessage('Wir beide müssen hier Rechte zum löschen haben. Mindestens einer hat sie nicht.');
     }
     const user = msg.mentions.users.first() || this.bot.users.get(params[0]);
     let amount = parseInt(params.pop());
-    if (!amount) return msg.channel.sendMessage('Bitte eine Anzahl an, zu löschender Nachrichten angeben. (Optional auch einen Nutzer)');
+    if (!amount) return msg.channel.sendMessage('Bitte eine Anzahl an zu löschender Nachrichten angeben. (Optional auch einen Nutzer)');
     if (user) {
-      let messages = (await msg.channel.fetchMessages({ limit: amount }))
+      const messages = (await msg.channel.fetchMessages({ limit: amount }))
         .filter(m => m.author.id === user.id && m.deletable);
       if (!messages.size) return '';
-      if (messages.size === 1) await messages.first().delete();
-      else await msg.channel.bulkDelete(messages);
+      if (messages.size === 1) return messages.first().delete();
+      else return msg.channel.bulkDelete(messages);
     } else if (amount <= 1) {
-      await msg.delete();
+      return msg.delete();
     } else {
-      await msg.channel.bulkDelete(amount);
+      return msg.channel.bulkDelete(amount);
     }
   }
 
