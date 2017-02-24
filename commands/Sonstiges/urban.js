80d3bfeebc54e6a93410eb8788c020c447669b9b
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-24 21:14:40 +0100

    tweaked urban.js

--- commands/Sonstiges/urban.js
+++ commands/Sonstiges/urban.js
@@ -11,44 +11,32 @@ module.exports = class Urban {
 
   async run(msg, params = []) {
     if (!params[0]) {
-      const mes = await msg.channel.sendEmbed({
-        title: 'Was soll den nachgeschlagen werden?',
-        description: 'Bedenke, dass mit `-n` (n ist eine Zahl)'
-        + '\nweitere Definitionen angezeigt werden können.',
-        fields: [
-          {
-            name: `\u200b`,
-            value: 'Antworte entweder mit `cancel` oder überlege länger als `30` Sekunden um diese Anfrage abzubrechen.',
-          }],
-        color: msg.member.highestRole.color,
-      });
-      try {
-        const collected = (await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000, errors: ['time'] })).first();
-        if (!collected) msg.delete();
-        mes.delete();
-        if (collected.content === 'cancel') {
-          collected.first().delete();
-          msg.delete();
-        } else if (collected.content.split(' ')[0].match(/^-\d+$/g)) {
-          this.query(msg, params.slice(1), params[0].replace('-', ''));
-        } else {
-          this.query(msg, collected.content.split(' '), 1);
-        }
-      } catch (e) {
-        mes.delete();
+      const mes = await msg.channel.sendEmbed(new this.bot.methods.Embed().setTitle('Was soll den nachgeschlagen werden?')
+        .setDescription('Bedenke, dass mit `-n` (n als Zahl)\nweitere Definitionen angezeigt werden können.')
+        .addField('\u200b', 'Antworte entweder mit `cancel` oder überlege länger als `30` Sekunden um diese Anfrage abzubrechen.')
+        .setColor(msg.member.color()));
+      const collected = (await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000 })).first();
+      mes.delete();
+      if (!collected) return msg.delete();
+      if (collected.content === 'cancel') {
+        collected.first().delete();
+        return msg.delete();
+      } else if (collected.content.split(' ')[0].match(/^-\d+$/g)) {
+        return this.query(msg, params.slice(1), params[0].replace('-', ''));
+      } else {
+        return this.query(msg, collected.content.split(' '), 1);
       }
     } else if (params[0].match(/^-\d+$/g)) {
-      this.query(msg, params.slice(1), params[0].replace('-', ''));
+      return this.query(msg, params.slice(1), params[0].replace('-', ''));
     } else {
-      this.query(msg, params, 1);
+      return this.query(msg, params, 1);
     }
   }
 
 
   async query(msg, params, definition) {
     const res = await request.get(`http://api.urbandictionary.com/v0/define?term=${params.join(' ')}`)
-      .send(null)
-      .set('Content-Type', 'application/json');
+      .send(null).set('Content-Type', 'application/json');
     definition = parseInt(definition) - 1;
     if (res.body.list.length === 0) {
       return msg.channel.sendEmbed(
@@ -75,19 +63,11 @@ module.exports = class Urban {
         .setTitle(`${params.join(' ')} [${definition + 1}/${res.body.list.length}]`)
         .setDescription('\u200b');
       const define = res.body.list[definition].definition.match(/(.|[\r\n]){1,1024}/g);
-      for (let i = 0; i < define.length; i++) {
-        e.addField(i === 0 ? 'Definition' : '\u200b',
-          define[i]);
-      }
+      for (let i = 0; i < define.length; i++) { e.addField(i === 0 ? 'Definition' : '\u200b', define[i]); }
       const example = res.body.list[definition].example.match(/(.|[\r\n]){1,1024}/g);
       if (example) {
-        for (let i = 0; i < example.length; i++) {
-          e.addField(i === 0 ? 'Beispiel' : '\u200b',
-            example[i]);
-        }
-      } else {
-        e.addField('\u200b', '\u200b');
-      }
+        for (let i = 0; i < example.length; i++) e.addField(i === 0 ? 'Beispiel' : '\u200b', example[i]);
+      } else { e.addField('\u200b', '\u200b'); }
       e.setFooter(`${msg.content} | Definition ${definition + 1} von insgesamt ${res.body.list.length} Definitionen.`, msg.author.avatarURL);
       return msg.channel.sendEmbed(e);
     }
