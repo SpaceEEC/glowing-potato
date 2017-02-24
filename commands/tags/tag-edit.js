f0bc16a378e3c42635667c74211785e59e8dd835
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-24 20:43:39 +0100

    tweaked tag-edit.js

--- commands/tags/tag-edit.js
+++ commands/tags/tag-edit.js
@@ -7,20 +7,18 @@ module.exports = class TagEdit {
   }
 
 
-  async run(msg, params = []) { // eslint-disable-line consistent-return
+  async run(msg, params = []) {
     if (!params[0] || (params[0] && !this.bot.internal.tags.has(`${msg.guild.id}|${params.join(' ')}`))) {
       const mes = await msg.channel.sendMessage('Bitte gib den Namen des zu ändernen Tags an.\n\nDiese Anfrage wird bei Eingabe von `cancel`, einer fehlerhaften Eingabe, oder nach 30 Sekunden abgebrochen.');
-      try {
-        const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000, errors: ['time'] });
-        if (this.bot.internal.tags.has(`${msg.guild.id}|${collected.first().content}`)) {
-          params[0] = collected.first().content;
-        } else {
-          mes.delete();
-          return msg.channel.sendMessage('Dieser Tag wurde nicht gefunden.');
-        }
-      } catch (e) {
+      const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000 });
+      if (!collected.size) {
         mes.delete();
         return msg.channel.sendMessage('Breche die Anfrage, wie durch die inaktivität gewünscht, ab.');
+      } else if (this.bot.internal.tags.has(`${msg.guild.id}|${collected.first().content}`)) {
+        params[0] = collected.first().content;
+      } else {
+        mes.delete();
+        return msg.channel.sendMessage('Dieser Tag wurde nicht gefunden.');
       }
     }
     if (this.bot.internal.tags.get(`${msg.guild.id}|${params.join(' ')}`).author === msg.author.id
@@ -29,29 +27,24 @@ module.exports = class TagEdit {
     }
     if (!params[1] || (params[1] && !this.bot.commands.get('tag').blacklist(msg, params.slice(1).join(' ')))) {
       const mes = await msg.channel.sendMessage('Bitte gib nun einen neuen Inhalt für diesen Tag an.\n\nDiese Anfrage wird bei Eingabe von `cancel`, einer fehlerhaften Eingabe, oder nach 30 Sekunden abgebrochen');
-      try {
-        const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000, errors: ['time'] });
-        if (collected.first().content === 'cancel') {
-          mes.delete();
-          return msg.channel.sendMessage('Breche Anfrage, wie gewünscht, ab.');
-        } else if (!this.bot.commands.get('tag').blacklist(msg, collected.first().content)) {
-          return msg.channel.sendMessage(`Diese Antwort enthält einen Link, welcher nicht hotlinkbar scheint, verlinke das Bild über einen hotlinkbaren Hoster.\n\nFalls dieses Bild hotlinkbar ist kontaktiere \`${this.bot.config.owner}\`.`);
-        } else {
-          params[1] = collected.first().content;
-        }
-      } catch (e) {
+      const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000 });
+      if (!collected.size) {
         mes.delete();
         return msg.channel.sendMessage('Breche die Anfrage, wie durch die inaktivität gewünscht, ab.');
+      } else if (collected.first().content === 'cancel') {
+        mes.delete();
+        return msg.channel.sendMessage('Breche Anfrage, wie gewünscht, ab.');
+      } else if (!this.bot.commands.get('tag').blacklist(msg, collected.first().content)) {
+        return msg.channel.sendMessage(`Diese Antwort enthält einen Link, welcher nicht hotlinkbar scheint, verlinke das Bild über einen hotlinkbaren Hoster.\n\nFalls dieses Bild hotlinkbar ist kontaktiere \`${this.bot.config.owner}\`.`);
+      } else {
+        params[1] = collected.first().content;
       }
     }
-    this.bot.debug(params[0].toLowerCase());
     return this.bot.internal.tag.edit(this.bot, msg.guild.id, params[0].toLowerCase(), params.slice(1).join(' '))
-      .then(() => {
-        msg.channel.sendMessage('Tag erfolgreich editiert!');
-      })
+      .then(() => msg.channel.sendMessage('Tag erfolgreich editiert!'))
       .catch((e) => {
         this.bot.err(`[tag-edit tag.edit()] ${e}`);
-        msg.channel.sendMessage('Es ist ein interner Fehler beim editieren dieses Tags aufgetreten.\nDer Tag wurde vielleicht editiert, bevor du ihn erneut zu editieren versuchst, prüfe ob der Tag editiert wurde.\n\nAndernfalls kontaktiere `spaceeec#0302`.');
+        msg.channel.sendMessage(`Es ist ein interner Fehler beim editieren dieses Tags aufgetreten.\nDer Tag wurde vielleicht editiert, bevor du ihn erneut zu editieren versuchst, prüfe ob der Tag editiert wurde.\n\nAndernfalls kontaktiere \`${this.bot.config.owner}\`.`);
       });
   }
 
