12a662acb80f38344acab0bbcd79ffbe865f8913
SpaceEEC <spaceeec@yahoo.com>
2017-02-24 19:42:21 +0100

    tweaked anime.js

--- commands/Weebstuff/anime.js
+++ commands/Weebstuff/anime.js
@@ -11,32 +11,22 @@ module.exports = class Anime {
 
   async run(msg, params = []) {
     if (!params[0]) {
-      const mes = await msg.channel.sendEmbed({
-        title: 'Ein kleines Missgeschick',
-        description: `\u200b
-Einen Anime suchen wir also... Wonach soll ich suchen?
-Ein Teil des Titels würde schon reichen.`,
-        fields: [
-          {
-            name: `\u200b`,
-            value: 'Antworte entweder mit `cancel` oder überlege länger als `30` Sekunden um diese Anfrage abzubrechen.',
-          }],
-        color: msg.member.highestRole.color,
-      });
+      const mes = await msg.channel.sendEmbed(new this.bot.methods.Embed().setTitle('Ein kleines Missgeschick')
+        .setDescription(`\u200b\nEinen Anime suchen wir also... Wonach soll ich suchen?\nEin Teil des Titels würde schon reichen.`)
+        .addField('\u200b', 'Antworte entweder mit `cancel` oder überlege länger als `30` Sekunden um diese Anfrage abzubrechen.')
+        .setColor(msg.member.color()));
       const collected = (await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000 })).first();
       mes.delete();
       if (!collected) return msg.delete();
       if (collected.content === 'cancel') {
         collected.delete();
         return msg.delete();
-      } else {
-        return this.authcheck(msg, collected.content.split(' '));
       }
+      params = collected.content.split(' ');
     }
     if (params.join(' ').includes('?')) {
       return msg.channel.sendEmbed(
-        new this.bot.methods.Embed()
-          .setColor(0xffff00)
+        new this.bot.methods.Embed().setColor(0xffff00)
           .setDescription('Bitte keine Fragezeichen verwenden, die Anfrage würde dadurch ungültig werden.')
       );
     }
@@ -47,49 +37,40 @@ Ein Teil des Titels würde schon reichen.`,
   async authcheck(msg, params) {
     if (this.bot.config.ani_expires <= Math.floor(Date.now() / 1000) + 300) {
       const message = await msg.channel.sendEmbed(
-        new this.bot.methods.Embed()
-          .setColor(0xffff00)
+        new this.bot.methods.Embed().setColor(0xffff00)
           .setDescription('Der Token ist ausgelaufen, ich fordere einen neuen an.\nDies kann einen Moment dauern, ich bitte um Geduld.'));
       const res = await request.post(`https://anilist.co/api/auth/access_token`)
         .send({
           grant_type: 'client_credentials',
           client_id: this.bot.internal.auth.anilist.client_id,
           client_secret: this.bot.internal.auth.anilist.client_secret,
-        })
-        .set('Content-Type', 'application/json');
+        }).set('Content-Type', 'application/json');
       this.bot.config.ani_token = res.body.access_token;
       this.bot.config.ani_expires = res.body.expires;
       this.bot.debug(`[anime] UPDATE config SET ani_expires=${res.body.expires}, ani_token=${res.body.access_token};`);
       await this.bot.db.run(`UPDATE config SET ani_expires=?, ani_token=?;`, [res.body.expires, res.body.access_token]);
       await message.edit({
-        embed: new this.bot.methods.Embed()
-          .setColor(0x00ff08)
+        embed: new this.bot.methods.Embed().setColor(0x00ff08)
           .setDescription('Token wurde erfolgreich erneuert.'),
       });
-      return this.query(params.join(' '), msg);
-    } else {
-      return this.query(params.join(' '), msg);
     }
+    return this.query(params.join(' '), msg);
   }
 
 
   async query(search, msg) {
     const res = await request.get(`https://anilist.co/api/${msg.cmd}/search/${search}?access_token=${this.bot.config.ani_token}`)
-      .send(null)
-      .set('Content-Type', 'application/json');
-    let response;
-    response = JSON.parse(res.text);
+      .send(null).set('Content-Type', 'application/json');
+    const response = JSON.parse(res.text);
     if (response.error) {
       if (response.error.messages[0] === 'No Results.') {
         return msg.channel.sendEmbed(
-          new this.bot.methods.Embed()
-            .setColor(0xffff00)
+          new this.bot.methods.Embed().setColor(0xffff00)
             .setDescription(`Leider keinen ${msg.cmd} auf diese Anfrage gefunden.`)
             .setFooter(`${msg.author.username}: ${msg.content}`, msg.author.avatarURL));
       } else {
         return msg.channel.sendEmbed(
-          new this.bot.methods.Embed()
-            .setColor(0xffff00)
+          new this.bot.methods.Embed().setColor(0xffff00)
             .setTitle('Unerwarteter Fehler:')
             .setDescription(`Sowas sollte nicht passieren.
 Bitte kontaktiere bitte \`${this.bot.config.owner}\`\n\n${response.error.messages[0]}`)
@@ -101,11 +82,12 @@ Bitte kontaktiere bitte \`${this.bot.config.owner}\`\n\n${response.error.message
       return this.getanswer(msg, response);
     }
   }
+
+
   async getanswer(msg, response) {
     let count = 1;
     const message = await msg.channel.sendEmbed(
-      new this.bot.methods.Embed()
-        .setColor(msg.member.color())
+      new this.bot.methods.Embed().setColor(msg.member.color())
         .setTitle(`Es gibt mehrere ${msg.cmd} auf diese Suchanfrage:`)
         .setDescription(response.map(r => `${count++}\t\t${r.title_english}`).join('\n'))
         .addField(`Für welchen ${msg.cmd} darf es denn die Info geben?`,
@@ -133,9 +115,7 @@ Bitte kontaktiere bitte \`${this.bot.config.owner}\`\n\n${response.error.message
 
 
   answer(response, msg, mes) {
-    const map = { amp: '&', lt: '<', gt: '>', quot: '"', '#039': "'" };
-    response.description = response.description.replace(/&([^;]+);/g, (m, c) => map[c])
-      .split('`').join('\'').split('<br>').join('\n'); // eslint-disable-line newline-per-chained-call
+    response.description = this.replaceMap(response.description, { '&amp;': '&', '&lt;': '<', '&gt;': '>', '&quot;': '"', '&#039;': "'", '`': '\'', '<br>': '\n', '<br />': '\n' });
     const embed = new this.bot.methods.Embed()
       .setColor(0x0800ff)
       .setTitle(response.title_japanese)
@@ -145,21 +125,12 @@ Bitte kontaktiere bitte \`${this.bot.config.owner}\`\n\n${response.error.message
       .setThumbnail(response.image_url_lge)
       .addField('Genres', response.genres.join(', '), true)
       .addField('Bewertung | Typ', `${response.average_score} | ${response.type}`, true);
-    if (msg.cmd === 'anime') {
-      embed.addField('Folgen', response.total_episodes, true);
-    } else {
-      embed.addField('Kapitel | Volumes',
-        `${response.total_chapters} | ${response.total_volumes}`,
-        true);
-    }
+    if (msg.cmd === 'anime') embed.addField('Folgen', response.total_episodes, true);
+    else embed.addField('Kapitel | Volumes', `${response.total_chapters} | ${response.total_volumes}`, true);
     if (response.start_date_fuzzy) {
       let title = 'Start';
       let value = this.formatFuzzy(response.start_date_fuzzy);
-      if (
-        (response.airing_status
-          && response.airing_status === 'finished airing')
-        || (response.publishing_status
-          && response.publishing_status === 'finished publishing')) {
+      if ((response.airing_status && response.airing_status === 'finished airing') || (response.publishing_status && response.publishing_status === 'finished publishing')) {
         title = 'Zeitraum';
         value += ` - ${response.end_date_fuzzy ? this.formatFuzzy(response.end_date_fuzzy) : `Nicht angegeben`}`;
       }
