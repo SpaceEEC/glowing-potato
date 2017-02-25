b11d6fbe868f8acf1274d1a182a936a75056166c
SpaceEEC <wermail2002-kap@yahoo.de>
2017-02-25 16:36:54 +0100

    async'ed pp.js

--- checks/pp.js
+++ checks/pp.js
@@ -1,31 +1,13 @@
-exports.run = (bot, msg, Command) => new Promise((resolve, reject) => {
+exports.run = async (bot, msg, Command) => {
   let permlvl = 1;
-  msg.guild.fetchMember(msg.author).then((member) => {
-    const modrole = msg.guild.roles.get(msg.conf.modrole);
-    if (modrole && member.roles.has(modrole.id)) {
-      permlvl = 5;
-    }
-    const adminrole = msg.guild.roles.get(msg.conf.adminrole);
-    if (adminrole && member.roles.has(adminrole.id)) {
-      permlvl = 10;
-    }
-    /* const users = bot.confs.get(msg.guild.id).users;
-    if (users[msg.author.id]) {
-      if (permlvl < users[msg.author.id].pp) {
-        permlvl = users[msg.author.id].pp;
-      }
-    } */
-    if (member === msg.guild.owner) {
-      permlvl = 11;
-    }
-    if (msg.author.id === bot.config.ownerID) {
-      permlvl = 12;
-    }
-    msg.permlvl = permlvl;
-    if (permlvl >= Command.conf.permLevel) resolve();
-    else reject(`Du darfst den Befehl \`${Command.help.name}\` leider nicht verwenden.`);
-  });
-});
+  const member = await msg.guild.fetchMember(msg.author);
+  if (member.roles.has(msg.conf.modrole)) permlvl = 5;
+  if (member.roles.has(msg.conf.adminrole)) permlvl = 10;
+  if (member === msg.guild.owner) permlvl = 11;
+  if (msg.author.id === bot.config.ownerID) permlvl = 12;
+  msg.permlvl = permlvl;
+  if (permlvl < Command.conf.permLevel) throw String(`Du darfst den Befehl \`${Command.help.name}\` leider nicht verwenden.`);
+};
 
 exports.conf = {
   spamProtection: false,
