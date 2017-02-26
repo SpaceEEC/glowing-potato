2b191f5238d61ddf95666f9b7bb6d62b0274e4fc
SpaceEEC <spaceeec@yahoo.com>
2017-02-26 14:07:24 +0100

    tag-add no longer allows empty messages and accepts images

--- commands/tags/tag-add.js
+++ commands/tags/tag-add.js
@@ -8,10 +8,14 @@ module.exports = class TagAdd {
 
 
   async run(msg, params = []) {
-    console.log(params);
     if (this.bot.commands.get('tag').conf.createLevel > msg.permlvl) return msg.channel.sendMessage('Du darfst leider keine Tags erstellen.');
-    if (!params[0] || (params[0] && ['add', 'edit', 'remove', 'list'].includes(params[0])) || (params[0] && this.bot.internal.tags.has(`${msg.guild.id}|${params.join(' ')}`))) {
-      const mes = await msg.channel.sendMessage('Bitte gib einen Namen für diesen neuen Tag an.\n\nDiese Anfrage wird bei Eingabe von `cancel`, einer fehlerhaften Eingabe, oder nach 30 Sekunden abgebrochen');
+    if (!params[0] || (params[0] && ['add', 'edit', 'remove', 'list'].includes(params[0])) || (params[0] && this.bot.internal.tags.has(`${msg.guild.id}|${params[0]}`))) {
+      let mes;
+      if (params[0] && this.bot.internal.tags.has(`${msg.guild.id}|${params[0]}`)) {
+        mes = await msg.channel.sendMessage('Dieser Tagname ist bereits belegt!\nBitte gib einen Namen für diesen neuen Tag an.\n\nDiese Anfrage wird bei Eingabe von `cancel`, einer fehlerhaften Eingabe, oder nach 30 Sekunden abgebrochen');
+      } else {
+        mes = await msg.channel.sendMessage('Bitte gib einen Namen für diesen neuen Tag an.\n\nDiese Anfrage wird bei Eingabe von `cancel`, einer fehlerhaften Eingabe, oder nach 30 Sekunden abgebrochen');
+      }
       const collected = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { maxMatches: 1, time: 30000 });
       if (!collected.size) {
         mes.delete();
@@ -34,12 +38,18 @@ module.exports = class TagAdd {
         return msg.channel.sendMessage('Breche Anfrage, wie gewünscht, ab.');
       } else if (!this.bot.commands.get('tag').blacklist(msg, collected.first().content)) {
         return msg.channel.sendMessage(`Diese Antwort enthält einen Link, welcher nicht hotlinkbar scheint, verlinke das Bild über einen hotlinkbaren Hoster.\n\nFalls dieses Bild hotlinkbar ist kontaktiere \`${this.bot.config.owner}\``);
-      } else { params[1] = collected.first().content; }
+      } else if (collected.first().attachments.first().height) {
+        params[1] = `${collected.first().content}\n${collected.first().attachments.first().url}`;
+      } else if (collected.first().content.length) {
+        params[1] = collected.first().content;
+      } else {
+        return msg.channel.send('Nachricht enthält weder Text noch Bild.');
+      }
     }
     return this.bot.internal.tag.add(this.bot, msg.guild.id, params[0].toLowerCase(), params.slice(1).join(' '), msg.author.id)
       .then(() => msg.channel.sendMessage('Tag erfolgreich erstellt!'))
       .catch((e) => {
-        this.bot.err(`[tag-add tag.add()] ${e}`);
+        this.bot.err(`[tag-add] [tag.add()] ${e}`);
         msg.channel.sendMessage(`Es ist ein interner Fehler beim Erstellen dieses Tags aufgetreten.\nDer Tag wurde vielleicht erstellt, bevor du es erneut zu erstellen versuchst, prüfe zunächst ob der Tag erstellt wurde.\n\nAndernfalls kontaktiere \`${this.bot.config.owner}\``);
       });
   }
