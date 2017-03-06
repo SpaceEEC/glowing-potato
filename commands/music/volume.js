fa156843ce3ea15dcfeaf3ea8bf532099e2a2896
SpaceEEC <wermail2002-kap@yahoo.de>
2017-03-06 01:22:28 +0100

    space, please proof read your messages, sincerly past space

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -31,13 +31,13 @@ module.exports = class VolumeCommand extends Command {
         .then((mes) => mes.delete(30000));
     }
     if (!queue.voiceChannel.members.has(msg.author.id)) {
-      return msg.say(`I am playing over here in ${queue.voiceChannel.name}, you are not here, so I the current volume will stay.`)
+      return msg.say(`I am playing over here in ${queue.voiceChannel.name}, you are not here, so the current volume will stay.`)
         .then((mes) => mes.delete(5000));
     }
 
     const volume = args;
     if (volume > 10 || volume < 0) {
-      return msg.say('Volumes goes from 1-10, my friend. Not higher not lower.')
+      return msg.say('Volumes goes from 1-10, my friend. Not higher nor lower.')
         .then((mes) => mes.delete(5000));
     }
     queue.volume = volume;
