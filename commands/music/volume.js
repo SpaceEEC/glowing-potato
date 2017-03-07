e3dbd1c88facf902a5e08035ed57710f2007c763
SpaceEEC <spaceeec@yahoo.com>
2017-03-07 02:46:12 +0100

    space, do you even commando args parsing?

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -7,6 +7,19 @@ module.exports = class VolumeCommand extends Command {
       group: 'music',
       memberName: 'volume',
       description: 'Sets the volume.',
+      examples: [
+        '`volume` Will display the current volume',
+        '`volume 2` Will set the volume to 2.',
+      ],
+      args: [
+        {
+          key: 'volume',
+          prompt: 'to which level would you like set the volume?\n',
+          type: 'integer',
+          min: 1,
+          max: 10
+        }
+      ],
       guildOnly: true,
     });
   }
@@ -19,14 +32,14 @@ module.exports = class VolumeCommand extends Command {
   }
 
   async run(msg, args) {
-    args = parseInt(args);
+    const volume = parseInt(args.volume);
     const queue = this.queue.get(msg.guild.id);
 
     if (!queue) {
       return msg.say('The queue is empty, no need to change the volume.')
         .then((mes) => mes.delete(5000));
     }
-    if (isNaN(args)) {
+    if (isNaN(volume)) {
       return msg.say(`The volume is \`${queue.volume}\`.`)
         .then((mes) => mes.delete(30000));
     }
@@ -35,11 +48,6 @@ module.exports = class VolumeCommand extends Command {
         .then((mes) => mes.delete(5000));
     }
 
-    const volume = args;
-    if (volume > 10 || volume < 0) {
-      return msg.say('Volumes goes from 1-10, my friend. Not higher nor lower.')
-        .then((mes) => mes.delete(5000));
-    }
     queue.volume = volume;
     if (queue.songs[0].dispatcher) queue.songs[0].dispatcher.setVolumeLogarithmic(volume / 5);
     return msg.say(`Volume is now \`${volume}\`.`)
