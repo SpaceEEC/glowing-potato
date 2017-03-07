1ecd18ea19e486812c24bc952c785c3e2e981a21
SpaceEEC <spaceeec@yahoo.com>
2017-03-07 02:55:39 +0100

    mark static functions as static

--- util/util.js
+++ util/util.js
@@ -2,6 +2,7 @@ module.exports = class UtilClass {
 
   /**
    * Replaces parts of a string determined by the specified map or object.
+   * @static
    * @param {string} input - The string that shall be replaced
    * @param {map|object} map - The map or object literal with keys and values to replace against.
    * @returns {string}
@@ -16,6 +17,7 @@ module.exports = class UtilClass {
 
   /**
    * Gets the used command (including aliases) from the message.
+   * @static
    * @param {message} msg The message to get the used command from.
    * @param {?object} object The optional object, containing key and value pairs to replace matches.
    * @returns {string}
@@ -27,6 +29,7 @@ module.exports = class UtilClass {
 
   /**
    * Returns either dataValues as an object or null if that row wasn't found.
+   * @static
    * @param {model} model The model to be quieried from
    * @param {object} object The object to be specified in the .findOne()
    * @returns {null|object}
