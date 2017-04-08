1829596fbfc63ebf4fb657606cb2e748fcfc4914
SpaceEEC <wermail2002-kap@yahoo.de>
2017-04-08 02:56:31 +0200

    Changes for prune.ts: - Handled the case of a single message. - Added the alias `clear`. - Reworded parts of the details

--- src/commands/moderation/prune.ts
+++ src/commands/moderation/prune.ts
@@ -6,13 +6,13 @@ export default class PruneCommand extends Command {
 	constructor(client: CommandoClient) {
 		super(client, {
 			name: 'prune',
-			aliases: ['purge', 'clean'],
+			aliases: ['purge', 'clean', 'clear'],
 			group: 'moderation',
 			memberName: 'prune',
 			description: 'Deletes messages.',
-			details: stripIndents`Let's you bulk delete messages.
+			details: stripIndents`Let's you delete multiple messages at once.
       		You can delete any amount between \`2\` and \`100\`.
-      		You can Mention, ID or Name a member to filter, so it will delete only whose messages.`,
+      		You can Mention, ID or Name a member to filter, so it will delete only their messages.`,
 			examples: [
 				'`prune 100 @spacebot` Will delete all messages from spacebot in the last 100 messages.',
 				'`prune 100 242685080693243906` Will delete all messages from the member with that ID in the last 100 messages.',
@@ -49,8 +49,8 @@ export default class PruneCommand extends Command {
 		}
 		let messages: Collection<string, Message> = await msg.channel.fetchMessages({ limit: args.amount });
 		if (args.member) messages = messages.filter((m: Message) => m.author.id === args.member.id);
-		if (!messages.size) return null;
-		await msg.channel.bulkDelete(messages);
-		return null;
+		if (!messages.size) return undefined;
+		if (messages.size === 1) await messages.first().delete();
+		else await msg.channel.bulkDelete(messages, true);
 	}
 };
