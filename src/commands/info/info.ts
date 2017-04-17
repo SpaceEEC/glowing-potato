75a4e01108e697de131f0144f3e79ce96053b394
SpaceEEC <spaceeec@yahoo.com>
2017-04-17 02:20:02 +0200

    humanize is somewhat bad, switched to moment-duration-format

--- src/commands/info/info.ts
+++ src/commands/info/info.ts
@@ -2,6 +2,7 @@ import { RichEmbed } from 'discord.js';
 import { Message } from 'discord.js';
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
 import * as moment from 'moment';
+import 'moment-duration-format';
 const { version }: { version: string } = require('../../../package.json');
 
 export default class InfoCommand extends Command {
@@ -19,7 +20,7 @@ export default class InfoCommand extends Command {
 		return msg.embed(new RichEmbed()
 			.setColor(0xffa500).setTitle('General informations about the bot.')
 			.setDescription('\u200b')
-			.addField('❯ Online for:', `• ${(moment.duration(this.client.uptime, 'milliseconds') as any).humanize()}`, true) //.format(' D[d] hh[h] mm[m] ss[s]')}`, true)
+			.addField('❯ Online for:', `• ${moment.duration(this.client.uptime).format('d[ days], h[ hours], m[ minutes and ]s[ seconds]')}`, true) //.format(' D[d] hh[h] mm[m] ss[s]')}`, true)
 			.addField('❯ Used memory:', `• ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
 			.addField('❯ Commands:', `• ${this.client.registry.commands.size}`, true)
 			//.addField('❯ spacebot-version:', `• v${version} ([glowing-potato](http://puu.sh/teDYW/d6f9555fbd.png))`, true)
