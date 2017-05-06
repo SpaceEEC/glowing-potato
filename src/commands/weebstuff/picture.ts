c6a12ade83d67a627d049e212d89e78a9adbbaf0
SpaceEEC <wermail2002-kap@yahoo.de>
2017-05-06 16:20:42 +0200

    Refactored picture.ts  - Using encodeUriCompenent  - desctructured snekfetch  - removed unnecessary regex  - only split once the search once  - removed unnecessary -0 and + 0

--- src/commands/weebstuff/picture.ts
+++ src/commands/weebstuff/picture.ts
@@ -3,7 +3,7 @@ import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
 
 import Util from '../../util/util';
 
-const snekfetch: any = require('snekfetch');
+const { get }: { get: any } = require('snekfetch');
 
 type post = {
 	id: number,
@@ -22,38 +22,43 @@ export default class PictureCommand extends Command {
 			guildOnly: true,
 			examples: [
 				'`konachan polychromatic` Displays a random picture with that tag from konachan.net',
-				'`donmai komeiji_satori` Displays a random picture with that tag from safebooru.donmai.us'
+				'`donmai komeiji_satori` Displays a random picture with that tag from safebooru.donmai.us',
 			],
 			args: [
 				{
 					key: 'search',
 					prompt: 'which tags shall be specified?\n',
 					type: 'string',
+					default: '',
+					parse: (value: string) => encodeURIComponent(value).replace(/%20/g, '+'),
 				}
-			]
+			],
 		});
 	}
 
 	public async run(msg: CommandMessage, args: { search: string, cmd: string }): Promise<Message | Message[]> {
-		if (!args.search.match(/^[a-z0-9_=()!-:.]+$/i)) {
-			return msg.embed(new RichEmbed().setColor(0xff0000)
-				.setColor(0xff0000)
-				.setDescription('Not allowed char in the search!')
-				.addField('Allowed chars:', 'A-z 0-9 _ = () ! - : .', true));
-		}
-
 		args.cmd = Util.getUsedAlias(msg, { pic: 'picture' });
+		const tagAmount: number = args.search.split('+').length;
+
+		if (tagAmount > 5) {
+			return msg.say('You can not search for more than 5 tags.');
+		}
 
 		if (args.cmd === 'picture') {
-			if (args.search.split(' ')[2]) args.cmd = 'konachan';
+			if (tagAmount > 2) args.cmd = 'konachan';
 			else args.cmd = ['konachan', 'donmai'][Math.floor(Math.random() * 2)];
+		} else if (args.cmd === 'donmai' && tagAmount > 2) {
+			return msg.say('You can not search donmai with more than two tags.');
 		}
-		if (args.cmd === 'konachan') this._konachan(msg, args.search.split(' ').join('+'));
-		else this._donmai(msg, args.search.split(' ').join('+'));
+
+		if (args.cmd === 'konachan') return this._konachan(msg, args.search.split(' ').join('+'));
+		else if (args.cmd === 'donmai') return this._donmai(msg, args.search.split(' ').join('+'));
+		throw new Error(`Unknown provider: ${args.cmd}`);
 	}
 
 	private async _konachan(msg: CommandMessage, search: string): Promise<Message | Message[]> {
-		const { body: posts }: { body: post[] } = await snekfetch.get(`http://konachan.com/post.json?tags=${`${search}+rating:s&limit=100`}`);
+		const { body: posts }: { body: post[] } = await get(`http://konachan.com/post.json?tags=${search}+rating:s&limit=100`);
+
 		if (posts.length === 0) {
 			return msg.embed(new RichEmbed().setColor(0xFFFF00)
 				.setAuthor('konachan.net', 'http://konachan.net/', 'http://konachan.net/favicon.ico')
@@ -61,14 +66,14 @@ export default class PictureCommand extends Command {
 				.addField('Search:', `[Link](http://konachan.net/post?tags=${search})`));
 		}
 
-		const post: post = posts[Math.floor(Math.random() * (posts.length - 0)) + 0];
+		const post: post = posts[Math.floor(Math.random() * posts.length)];
 		return msg.embed(new RichEmbed()
 			.setColor(msg.member.displayColor).setImage(`http:${post.sample_url}`)
 			.setDescription(`[Source](http://konachan.net/post/show/${post.id})`));
 	}
 
 	private async _donmai(msg: CommandMessage, search: string): Promise<Message | Message[]> {
-		const { body: posts }: { body: post[] } = await snekfetch.get(`http://safebooru.donmai.us/posts.json?limit=1&random=true&tags=${search}`);
+		const { body: posts }: { body: post[] } = await get(`http://safebooru.donmai.us/posts.json?limit=1&random=true&tags=${search}`);
 
 		if (posts.length === 0) {
 			return msg.embed(new RichEmbed().setColor(0xFFFF00)
