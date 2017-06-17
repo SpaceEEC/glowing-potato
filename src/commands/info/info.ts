87c2a2992a773d5f04d2f1f2481a15b5efe80262
SpaceEEC <spaceeec@yahoo.com>
2017-06-17 17:24:10 +0200

    Replaced commands field with version and renamed some variables and small code formatting changes

--- src/commands/info/info.ts
+++ src/commands/info/info.ts
@@ -1,9 +1,11 @@
-import { Message, RichEmbed, version as vdjs } from 'discord.js';
+import { stripIndents } from 'common-tags';
+import { Message, RichEmbed, version as libVersion } from 'discord.js';
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
 import * as moment from 'moment';
 import 'moment-duration-format';
 
-const { version: vcom }: { version: string } = require('discord.js-commando');
+const { version: commandoVersion }: { version: string } = require('discord.js-commando');
+const { version }: { version: string } = require('../../../package.json');
 
 export default class InfoCommand extends Command {
 	public constructor(client: CommandoClient) {
@@ -20,16 +22,19 @@ export default class InfoCommand extends Command {
 		return msg.embed(new RichEmbed()
 			.setColor(0xffa500).setTitle('General information about the bot.')
 			.setDescription('\u200b')
-			.addField('❯ Uptime:', `• ${(moment.duration(this.client.uptime) as any)
-				.format('d[ days], h[ hours], m[ minutes and ]s[ seconds]')}`)
+			.addField('❯ Uptime:',
+			`• ${(moment.duration(this.client.uptime) as any).format('d[ days], h[ hours], m[ minutes and ]s[ seconds]')}`)
 			.addField('❯ Memory in use:', `• ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
-			.addField('❯ Commands:', `• ${this.client.registry.commands.size}`, true)
-			// .addField('❯ spacebot-version:', `• v${version} ([glowing-potato](http://puu.sh/teDYW/d6f9555fbd.png))`, true)
+			// .addField('❯ Commands:', `• ${this.client.registry.commands.size}`, true)
+			.addField('❯ spacebot-version:', `• v${version} ([glowing-potato](http://puu.sh/teDYW/d6f9555fbd.png))`, true)
 			.addField('❯ Shitcode repositorie:', '• [GitHub](https://github.com/SpaceEEC/glowing-potato)', true)
 			.addField('❯ Responsible:', '• `space#0302`', true)
-			.addField('❯ Library: ', `• [discord.js](https://github.com/hydrabolt/discord.js)\n• Version: ${vdjs}`, true)
-			.addField('❯ Framework: ',
-				`• [discord.js-commando](https://github.com/Gawdl3y/discord.js-commando)\n• Version: ${vcom}`, true)
+			.addField('❯ Library: ', stripIndents`
+			• [discord.js](https://github.com/hydrabolt/discord.js)
+			• Version: ${libVersion}`, true)
+			.addField('❯ Framework: ', stripIndents`
+			• [discord.js-commando](https://github.com/Gawdl3y/discord.js-commando)
+			• Version: ${commandoVersion}`, true)
 			.setTimestamp()
 			.setThumbnail(this.client.user.displayAvatarURL)
 			.setFooter(msg.cleanContent, msg.author.displayAvatarURL));
