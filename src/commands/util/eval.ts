63f2cb1f7f416894bcc42dfeda51a5807edef5cb
SpaceEEC <spaceeec@yahoo.com>
2017-06-25 18:02:03 +0200

    Made the eval command less cluttered and removed unnecessary output

--- src/commands/util/eval.ts
+++ src/commands/util/eval.ts
@@ -1,8 +1,12 @@
-import { stripIndents } from 'common-tags';
-import { Message } from 'discord.js';
+import { DiscordAPIError, Message } from 'discord.js';
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
+import { inspect } from 'util';
 
+import { stripIndents } from 'common-tags';
 import { Util } from '../../util/util.js';
+
+// tslint:disable-next-line:variable-name
+const RealDiscordAPIError: typeof DiscordAPIError = require('discord.js/src/client/rest/DiscordAPIError.js');
 
 export default class EvalCommand extends Command {
 	public constructor(client: CommandoClient) {
@@ -30,39 +34,50 @@ export default class EvalCommand extends Command {
 	public async run(msg: CommandMessage, args: { code: string }): Promise<Message | Message[]> {
 		const time: number = Date.now();
 		try {
-			let evaled: any;
 			if (Util.getUsedAlias(msg) === 'async') args.code = `(async()=>{${args.code}})();`;
+
+			let evaled: any;
 			// tslint:disable-next-line:no-eval
 			evaled = await Promise.resolve(eval(args.code));
-			const responseTypeof: string = typeof evaled;
-			if (typeof evaled !== 'string') { evaled = require('util').inspect(evaled, false, 0); }
-			if (evaled.includes(this.client.token)) {
-				return msg.say('The token does not belong in here. 👀');
-			}
-			return msg.say(stripIndents`
-				\`code:\`
-				\`\`\`js\n${args.code ? args.code.split(`\``).join(`´`) : 'falsy'}\`\`\`
-				\`evaled\\returned:\`
-				\`\`\`js\n${evaled ? evaled.split(`\``).join(`´`) : 'falsy'}\`\`\`
-				\`typeof:\`
-				\`\`\`js\n${responseTypeof}
-				\`\`\`\n
+
+			const typeofEvaled: string = typeof evaled;
+			if (typeofEvaled !== 'string') evaled = inspect(evaled, false, 0);
+			if (evaled.includes(this.client.token)) return msg.say('The token does not belong in here. 👀');
+
+			return msg.say(stripIndents
+				`\`returned\` \`typeof:\` \`${typeofEvaled}\`
+				\`\`\`js
+				${evaled ? evaled.split('`').join('´') : 'falsy'}
+				\`\`\`
 				Took \`${Date.now() - time}\`ms`,
 			).catch((e: Error) =>
-				msg.say(stripIndents`
-					Fehler beim Senden der Antwort:\n
+				msg.say(stripIndents
+					`Fehler beim Senden der Antwort:
+
 					\`\`\`js
 					${e.stack ? e.stack : e}
 					\`\`\``,
 				),
 			);
 		} catch (error) {
-			return msg.say(stripIndents`\`E-ROHR\`
-			\`\`\`js
-			${error.url ? `${error.status} ${error.statusText}\n${error.text}` : error}
-			\`\`\`\n
-			Took \`${Date.now() - time}\`ms`,
-			);
+			if (error instanceof RealDiscordAPIError) {
+				return msg.say(stripIndents
+					`\`E-ROHR\` \`Code\` \`${error.code}\`
+
+					\`\`\`js
+					${error.message}
+					\`\`\`
+					Took \`${Date.now() - time}\`ms`,
+				);
+			} else {
+				return msg.say(stripIndents
+					`\`E-ROHR\`
+					\`\`\`js
+					${inspect(error, false, 0)}
+					\`\`\`
+					Took \`${Date.now() - time}\`ms`,
+				);
+			}
 		}
 	}
 }
