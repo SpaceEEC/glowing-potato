e46b84795e6d930ca4f2ca4768d4647bf6667aee
SpaceEEC <wermail2002-kap@yahoo.de>
2017-06-28 13:04:42 +0200

    clean up for rewrite

--- src/dataProviders/SQLite.ts
+++
@@ -1,8 +0,0 @@
-import * as sqlize from 'sequelize';
-
-export const sequelize: sqlize.Sequelize = new (sqlize as any)('database', 'username', 'password', {
-	dialect: 'sqlite',
-	host: 'localhost',
-	logging: false,
-	storage: '../settings.sqlite',
-});
