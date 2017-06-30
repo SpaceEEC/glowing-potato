f0e5362c4fb44fe3ea8424bb13822d33d60e7d75
SpaceEEC <wermail2002-kap@yahoo.de>
2017-07-01 01:24:30 +0200

    Committing "misc" commands

---
+++ src/commands/misc/Avatar.ts
@@ -0,0 +1,42 @@
+import { FileOptions, RichEmbed, User } from 'discord.js';
+import { Message } from 'yamdbf/bin';
+import { clientPermissions, desc, group, guildOnly, name, usage, using } from 'yamdbf/bin/command/CommandDecorators';
+import { expect } from 'yamdbf/bin/command/middleware/Expect';
+import { resolve } from 'yamdbf/bin/command/middleware/Resolve';
+
+import { Client } from '../../structures/Client';
+import { Command } from '../../structures/Command';
+import { ReportError } from '../../structures/ReportError';
+
+@clientPermissions('SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES')
+@desc('Gets an user\'s avatar')
+@name('avatar')
+@group('misc')
+@guildOnly
+@usage('<prefix>avatar <User>')
+export default class AvatarCommand extends Command<Client>
+{
+	@using(resolve({ '<User>': 'User' }))
+	@using(expect({ '<User>': 'User' }))
+	@ReportError
+	public async action(message: Message, [user]: [User]): Promise<void>
+	{
+		const name: string = (user.avatar && user.avatar.startsWith('a_'))
+			? 'user.gif'
+			: 'user.png';
+
+		const fileOptions: FileOptions = {
+			attachment: user.displayAvatarURL,
+			name,
+		};
+
+		return message.channel.send(
+			{
+				embed: new RichEmbed()
+					.setColor(message.member.displayColor)
+					.attachFile(fileOptions)
+					.setImage(`attachment://${name}`),
+			},
+		).then(() => undefined);
+	}
+}
---
+++ src/commands/misc/Urban.ts
@@ -0,0 +1,85 @@
+import { get, Result } from 'snekfetch';
+import { Message } from 'yamdbf/bin';
+import { clientPermissions, desc, group, guildOnly, name, usage, using } from 'yamdbf/bin/command/CommandDecorators';
+
+import { Client } from '../../structures/Client';
+import { Command } from '../../structures/Command';
+import { ReportError } from '../../structures/ReportError';
+import { RichEmbed } from '../../structures/RichEmbed';
+import { ProbablyNotABuffer } from '../../types/ProbablyNotABuffer';
+import { UrbanDefinition } from '../../types/UrbanDefinition';
+import { UrbanResponse } from '../../types/UrbanResponse';
+
+@clientPermissions('SEND_MESSAGES', 'EMBED_LINKS')
+@desc('Displays a definition from the urbandictionary.')
+@name('urban')
+@group('misg')
+@guildOnly
+@usage('<prefix>urban [-n] <...search>')
+export default class Urban extends Command<Client>
+{
+	@using((message: Message, args: string[]) =>
+	{
+		let selectedNumber: number = 0;
+		if (args[0].match(/^-\d+$/g))
+		{
+			selectedNumber = parseInt(args.shift().slice(1)) - 1;
+		}
+		return [message, [selectedNumber, args]];
+	})
+	@ReportError
+	public async action(message: Message, [selectedNumber, search]: [number, string[]]): Promise<void>
+	{
+		const query: string = encodeURIComponent(search.join('+')).replace(/%2B/g, '+');
+
+		const body: UrbanResponse = await get(`http://api.urbandictionary.com/v0/define?term=${query}`)
+			.then((response: Result) => response.body as ProbablyNotABuffer);
+
+		if (!body.list.length)
+		{
+			return message.channel.send(
+				{
+					embed:
+					new RichEmbed()
+						.setColor(0x1d2439)
+						.setAuthor('Urbandictionary',
+						'http://www.urbandictionary.com/favicon.ico',
+						'http://www.urbandictionary.com/')
+						// tslint:disable-next-line:max-line-length
+						.setThumbnail('https://cdn.discordapp.com/attachments/242641288397062145/308943250465751041/eyJ1cmwiOiJodHRwOi8vaS5pbWd1ci5jb20vQ2NJWlpzYS5wbmcifQ.png')
+						.addField('No results', 'Maybe made a typo?')
+						.addField('Search:', `[URL](http://www.urbandictionary.com/define.php?term=${query})`)
+						.setFooter(message.cleanContent, message.author.displayAvatarURL),
+				},
+			).then(() => undefined);
+		}
+
+		if (body.list.length < selectedNumber + 1) selectedNumber = body.list.length - 1;
+
+		const embed: RichEmbed = new RichEmbed()
+			.setColor(0x1d2439)
+			.setAuthor('Urbandictionary',
+			'http://www.urbandictionary.com/favicon.ico',
+			'http://www.urbandictionary.com/')
+			// tslint:disable-next-line:max-line-length
+			.setThumbnail('https://cdn.discordapp.com/attachments/242641288397062145/308943250465751041/eyJ1cmwiOiJodHRwOi8vaS5pbWd1ci5jb20vQ2NJWlpzYS5wbmcifQ.png')
+			.setTitle(`${search.join(' ')} [${selectedNumber + 1}/${body.list.length}]`)
+			.setDescription('\u200b');
+
+		const { example, definition }: UrbanDefinition = body.list[selectedNumber];
+
+		embed.splitToFields('Defition', definition);
+
+		if (example)
+		{
+			embed.splitToFields('Example', example);
+		}
+
+		embed.setFooter(`${message.cleanContent} | Definition ${selectedNumber + 1} from ${body.list.length} Definitions.`,
+			message.author.displayAvatarURL);
+
+		return message.channel.send({ embed })
+			.then(() => undefined);
+	}
+
+}
