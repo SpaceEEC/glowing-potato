74d1c78f2e9fdf3bb9373ed20542a3be9e8ba50e
SpaceEEC <wermail2002-kap@yahoo.de>
2017-06-30 21:03:00 +0200

    committing info commands

---
+++ src/commands/info/GuildInfo.ts
@@ -0,0 +1,172 @@
+import {
+	Collection,
+	Emoji,
+	Invite,
+	PartialGuild,
+	PartialGuildChannel,
+	RichEmbed,
+	Role,
+	SnowflakeUtil,
+} from 'discord.js';
+import * as moment from 'moment';
+import { Command, Guild, Message } from 'yamdbf/bin';
+import { aliases, clientPermissions, desc, group, guildOnly, name, usage } from 'yamdbf/bin/command/CommandDecorators';
+
+import { Client } from '../../structures/Client';
+import { ReportError } from '../../structures/ReportError';
+
+// tslint:disable-next-line:variable-name
+const { Endpoints }: { Endpoints: any } = require('discord.js').Constants;
+
+@aliases('guild')
+@clientPermissions('SEND_MESSAGES', 'EMBED_LINKS')
+@desc('Displays info for the current or specified guild.')
+@name('guildinfo')
+@group('info')
+@guildOnly
+@usage('<prefix>guildinfo [ID | Invite]')
+export default class GuildInfo extends Command<Client>
+{
+	public constructor(client: Client) { super(); }
+
+	@ReportError
+	public action(message: Message, [input]: [string]): Promise<void>
+	{
+		return this._resolveGuild(message, input)
+			.catch(() => message.channel.send('Could not resolve the provided ID or invite code to a guild.'))
+			.then(() => undefined);
+	}
+
+	private _sendFullGuild(message: Message, guild: Guild): Promise<void>
+	{
+		const channels: {
+			// just for typescript
+			dm?: number,
+			group?: number,
+			// those are never present in a guild
+
+			text: number,
+			voice: number,
+		} = { text: 0, voice: 0 };
+
+		for (const channel of guild.channels.values())
+		{
+			++channels[channel.type];
+		}
+
+		const roles: string = guild.roles
+			.reduce((acc: string[], role: Role) =>
+			{
+				if (role.id !== message.guild.id)
+				{
+					acc.push(role.toString());
+				}
+				return acc;
+			},
+			[],
+		).join(', ');
+
+		const created: moment.Moment = moment(guild.createdTimestamp);
+
+		const embed: RichEmbed = new RichEmbed()
+			.setColor(0xffa500)
+			.setDescription('\u200b')
+			.setTitle(`Information about ${guild.name}`)
+			.setThumbnail(guild.iconURL)
+			.addField('❯ Channel:',
+			`• \`${channels.text}\` text channels\n`
+			+ `• \`${channels.voice}\` voice channels`, true)
+			.addField('❯ Member:', `\`${guild.memberCount}\` total members\n`
+			+ `Owner is ${guild.owner}`, true)
+			.addField('❯ Created:',
+			`Roughly ${created.utc().fromNow()}\n`
+			+ `(${created.format('DD.MM.YYYY hh:mm:ss [[UTC]]')})`, true)
+			.addField('❯ Region:',
+			guild.region[0].toUpperCase() + guild.region.slice(1), true)
+			.addField('❯ Roles:',
+			`• \`${guild.roles.size}\`: ${roles}`.slice(0, 1024), true)
+			.addField('❯ Emojis:', this._getRandomEmojis(guild.emojis) || '`none`', true)
+			.setTimestamp()
+			.setFooter(message.cleanContent, message.author.displayAvatarURL);
+
+		return message.channel.send({ embed })
+			.then(() => undefined);
+
+	}
+
+	private _sendPartialGuild(message: Message, { channel, guild, memberCount, presenceCount }:
+		{
+			channel: PartialGuildChannel,
+			guild: PartialGuild,
+			memberCount: number,
+			presenceCount: number,
+		}): Promise<void>
+	{
+		const iconURL: string = guild.icon
+			? Endpoints.Guild(guild).Icon((this.client.options as any).http.cdn, guild.icon)
+			: null;
+
+		const created: moment.Moment = moment(SnowflakeUtil.deconstruct(guild.id).timestamp);
+
+		const embed: RichEmbed = new RichEmbed()
+			.setColor(0xffa500)
+			.setDescription('\u200b')
+			.setTitle(`(Partial Guild) Information about ${guild.name}`)
+			.setThumbnail(iconURL)
+			.addField('❯ Default Channel:', channel.name, true)
+			.addField('❯ Online / Total Members:',
+			`${presenceCount} / ${memberCount}`, true)
+			.addField('❯ Created:',
+			`Roughly ${created.utc().fromNow()}\n`
+			+ `(${created.format('DD.MM.YYYY hh:mm:ss [[UTC]]')})`, true)
+			.setTimestamp()
+			.setFooter(message.cleanContent, message.author.displayAvatarURL);
+
+		return message.channel.send({ embed })
+			.then(() => undefined);
+	}
+
+	private _resolveGuild(message: Message, input: string): Promise<void>
+	{
+		if (!input) return this._sendFullGuild(message, message.guild);
+
+		if (this.client.guilds.has(input))
+		{
+			return this._sendFullGuild(message, this.client.guilds.get(input));
+		}
+
+		return this.client.fetchInvite(input)
+			.then((invite: Invite) =>
+			{
+				if (invite.guild instanceof Guild) return this._sendFullGuild(message, invite.guild);
+				return this._sendPartialGuild(message, invite);
+			},
+		);
+	}
+
+	private _getRandomEmojis(emojiCollection: Collection<string, Emoji>): string
+	{
+		const emojis: string[] = emojiCollection.map((e: Emoji) => e.toString());
+
+		for (let i: number = emojis.length - 1; i > 0; --i)
+		{
+			const r: number = Math.floor(Math.random() * (i + 1));
+			const t: string = emojis[i];
+			emojis[i] = emojis[r];
+			emojis[r] = t;
+		}
+
+		let mappedEmojis: string = '';
+		for (const emoji of emojis)
+		{
+			if (mappedEmojis.length + emoji.length > 1021)
+			{
+				mappedEmojis += '...';
+				break;
+			}
+			mappedEmojis += ` ${emoji}`;
+		}
+
+		return mappedEmojis;
+	}
+}
---
+++ src/commands/info/Info.ts
@@ -0,0 +1,75 @@
+import { RichEmbed, User, version as libVersion } from 'discord.js';
+import * as moment from 'moment';
+import 'moment-duration-format';
+import { Command, Message, version as YAMDBFVersion } from 'yamdbf/bin';
+import { aliases, clientPermissions, desc, group, guildOnly, name, usage } from 'yamdbf/bin/command/CommandDecorators';
+
+import { Client } from '../../structures/Client';
+import { ReportError } from '../../structures/ReportError';
+
+const { version }: { version: string } = require('../../../package.json');
+
+@aliases('stats')
+@clientPermissions('SEND_MESSAGES', 'EMBED_LINKS')
+@desc('Displays generic information about the bot')
+@name('info')
+@group('info')
+@guildOnly
+@usage('<prefix>info')
+export default class InfoCommand extends Command<Client>
+{
+	public constructor(client: Client) { super(); }
+
+	@ReportError
+	public async action(message: Message): Promise<void>
+	{
+		let owners: string = '';
+		if (this.client.owner instanceof Array)
+		{
+			const ownerArray: User[] = await Promise.all(
+				this.client.owner.map((owner: string) =>
+					this.client.users.get(owner) || this.client.fetchUser(owner),
+				),
+			);
+			owners = ownerArray.map((owner: User) => `\`${owner.tag}\``).join(', ');
+		}
+		else
+		{
+			owners = `\`${(this.client.users.get(this.client.owner) || await this.client.fetchUser(this.client.owner)).tag}\``;
+		}
+
+		const embed: RichEmbed = new RichEmbed()
+			.setColor(0xffa500)
+			.setTitle('General information about the bot')
+			.addField('❯ Uptime:',
+			`• ${(moment.duration(this.client.uptime) as any).format('d[ days], h[ hours], m[ minutes and ]s[ seconds]')}`,
+			false)
+			.addField('❯ Memory in use:',
+			`• ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+			.addField('❯ spacebot-version:',
+			`• v${version}\n`
+			+ '  ([glowing-potato](http://puu.sh/teDYW/d6f9555fbd.png))', true)
+			.addField('❯ Repositorie:',
+			'• [GitHub](https://github.com/SpaceEEC/glowing-potato)', true)
+			.addField('❯ Responsible:',
+			`• ${owners}`, true);
+
+		if (this.client.voiceConnections.size)
+		{
+			embed.addField('❯ Active voice connections:', this.client.voiceConnections.size, false);
+		}
+
+		embed.addField('❯ Library: ',
+			'• [discord.js](https://github.com/hydrabolt/discord.js)\n'
+			+ `  Version: ${libVersion}`, true)
+			.addField('❯ Framework: ',
+			'• [YAMDBF](https://github.com/zajrik/yamdbf)\n'
+			+ `  Version: ${YAMDBFVersion}`, true)
+			.setTimestamp()
+			.setThumbnail(this.client.user.displayAvatarURL)
+			.setFooter(message.cleanContent, message.author.displayAvatarURL);
+
+		return message.channel.send({ embed })
+			.then(() => undefined);
+	}
+}
