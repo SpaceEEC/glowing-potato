5942af59b75a4d72c4c1f5c1df47b57ec429f606
SpaceEEC <wermail2002-kap@yahoo.de>
2017-07-01 00:07:55 +0200

    sorted AnimeData, CharData and MangaData alphabetically

--- src/types/CharData.ts
+++ src/types/CharData.ts
@@ -1,11 +1,11 @@
 export type CharData = {
-	name_alt: string;
-	info: string;
 	id: number;
-	name_first: string;
-	name_last: string;
-	name_japanese: string;
 	image_url_lge: string;
 	image_url_med: string;
+	info: string;
+	name_alt: string;
+	name_first: string;
+	name_japanese: string;
+	name_last: string;
 	role: string;
 };
