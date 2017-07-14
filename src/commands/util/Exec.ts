7a8a6f5737297076a10bed3c19d36f35a4c158dc
SpaceEEC <wermail2002-kap@yahoo.de>
2017-07-14 14:25:40 +0200

    Fixed error thrown when ExecCommand exceeds max char limit

--- src/commands/util/Exec.ts
+++ src/commands/util/Exec.ts
@@ -25,10 +25,16 @@ export default class ExecCommand extends Command<Client>
 		const { error, stdout, stderr }: { error?: any, stdout?: string, stderr?: string } = await execAsync(code.join(' '))
 			.catch((err: any) => ({ error: err, stdout: err.stdout, stderr: err.stderr }));
 
-		const response: string = '`EXEC` '
+		let response: string = '`EXEC` '
 			+ (error && error.code ? `\`Error Code: ${error.code}\`\n\n` : '')
 			+ (stdout ? `\`STDOUT\`\n\`\`\`xl\n${stdout}\`\`\`\n\n` : '')
 			+ (stderr ? `\`STDERR\`\n\`\`\`xl\n${stderr}\`\`\`\n\n` : '');
+
+		// might be replaced with a hastebing or something similar in the future
+		if (response.length > 2000)
+		{
+			response = `${response.slice(0, 1997)}...`;
+		}
 
 		return message.channel.send(response).then(() => undefined);
 	}
