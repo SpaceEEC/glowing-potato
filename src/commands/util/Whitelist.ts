8dafa76e6d15c4784bcc2b43524e061683b673f8
SpaceEEC <spaceeec@yahoo.com>
2017-08-07 17:39:26 +0200

    Temporarily recreated the whitelist command and updated yamdbf and yamdbf-lang-german
    The command has been removed with https://github.com/zajrik/yamdbf/commit/bc59e6813c7c6afcb7e9019791fe6af0ac231b53
    It will be finally removed in a future update, for now this will give the user a warning and an example on how to remove a user from the blacklist with the blacklist command.

---
+++ src/commands/util/Whitelist.ts
@@ -0,0 +1,32 @@
+import { CommandDecorators, Message, Middleware, ResourceLoader } from 'yamdbf';
+
+import { Client } from '../../structures/Client';
+import { Command } from '../../structures/Command';
+
+const { aliases, desc, name, usage, group, callerPermissions, localizable, using } = CommandDecorators;
+const { expect, resolve } = Middleware;
+
+@aliases('wl')
+@callerPermissions('ADMINISTRATOR')
+@desc('Deprecated/Obsolete! Remove a user from the command blacklist')
+@name('whitelist')
+@group('util')
+@usage('<prefix>whitelist <user> [\'global\']')
+export default class WhitelistCommand extends Command<Client>
+{
+	@using(resolve({ '<user>': 'User' }))
+	@using(expect({ '<user>': 'User' }))
+	@localizable
+	public async action(message: Message, [res]: [ResourceLoader]): Promise<void>
+	{
+		await message.channel.send(res('CMD_WHITELIST_DEPRECATED',
+			{
+				prefix: await message.guild.storage.settings.get('prefix'),
+			},
+		));
+
+		const [, user, global] = message.content.split(/ +/);
+
+		return this.client.commands.get('blacklist').action(message, ['remove', user, global]);
+	}
+}
