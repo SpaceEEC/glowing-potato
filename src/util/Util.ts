220cfc81c739604b2915102ac3023948271fdc0e
SpaceEEC <wermail2002-kap@yahoo.de>
2017-09-14 17:04:50 +0200

    fix(Util): do not double increment i when chunking an array

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -57,7 +57,7 @@ export class Util
 		const chunks: T[][] = [];
 		const length: number = Math.ceil(input.length / chunkSize);
 
-		for (let i: number = 0; i < length; ++i)
+		for (let i: number = 0; i < length; null)
 		{
 			chunks.push(input.slice(i * chunkSize, ++i * chunkSize));
 		}
